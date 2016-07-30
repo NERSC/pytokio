61d6fe69ab6cba0611a10535403e77248a2f9661
Glenn K. Lockwood <glock@lbl.gov>
2016-07-29 23:18:28 -0700

    added tokio.warning and tokio.error, used by the es module.  need to update lmt and hdf5 modules to also use this

--- tokio/__init__.py
+++ tokio/__init__.py
@@ -7,6 +7,24 @@ DEBUG = False
 LMT_TIMESTEP = 5
 
 def _debug_print( string ):
+    """
+    Print debug messages if the module's global debug flag is enabled.
+    """
     if DEBUG:
         sys.stderr.write( string + "\n" )
     return
+
+def error( string ):
+    """
+    Handle errors generated within TOKIO.  Currently just a passthrough to
+    stderr; should probably provide exceptions later on.
+    """
+    sys.stderr.write( string + "\n" )
+
+def warning( string ):
+    """
+    Handle warnings generated within TOKIO.  Currently just a passthrough to
+    stderr; should probably provide a more rigorous logging/reporting
+    interface later on.
+    """
+    sys.stderr.write( string + "\n" )
