67d495f5ab4bec1ce0f3cebe89bc352360d620fd
Glenn K. Lockwood <glock@lbl.gov>
2016-08-16 22:32:12 -0700

    creates the entire v1 schema in HDF5 now
    - creates old v1 datasets/groups under the "/version1" group and makes symlinks for backwards compatibility
    - all metadata is correctly populated on init
    - caved in and now create 17281 rows to match true v1 schema

--- tokio/hdf5.py
+++ tokio/hdf5.py
@@ -20,37 +20,88 @@ class HDF5(h5py.File):
         self.timestep = None
         super(HDF5,self).__init__( *args, **kwargs )
 
-    def init_datasets( self, num_osts, num_timesteps, host='unknown', filesystem='unknown' ):
+    def init_datasets( self, oss_names, ost_names, mds_op_names, num_timesteps, host='unknown', filesystem='unknown' ):
         """
         Create datasets if they do not exist, and set the appropriate attributes
         """
-        ### note that we do square chunking here because there are times when we
-        ### want to both iterate over time (rows), such as when collecting
-        ### aggregate file system metrics, as well as iterate over STs, such as
-        ### when trying to subselect STs participating in a specific job.
+        ### Notes:
+        ### 1. We do square chunking here because there are times when we
+        ###    want to both iterate over time (rows), such as when collecting
+        ###    aggregate file system metrics, as well as iterate over STs,
+        ###    such as when trying to subselect STs participating in a
+        ###    specific job.
+        ### 2. v1 has num_timesteps+1 for reasons unknown.  Strict v1 leaves
+        ###    the first timestep zero and includes the first timestep from
+        ###    the following day as the (num_timesteps+1)th row, but we
+        ###    simply leave the (num_timesteps+1)th zero and correctly populate
+        ###    the first datapoints of the day instead.
+        num_osses = len(oss_names)
+        num_osts = len(ost_names)
+        num_mds_ops = len(mds_op_names)
+        _V1_SCHEMA = {
+            '/FSMissingGroup/FSMissingDataSet' : {
+                'shape': (num_osses, num_timesteps+1),
+                'chunks': True,
+                'compression': 'gzip',
+                'dtype': 'i4',
+            },
+            '/FSStepsGroup/FSStepsDataSet' : {
+                'shape': (num_timesteps+1,),
+                'chunks': True,
+                'compression': 'gzip',
+                'dtype': 'i4',
+            },
+            '/MDSCPUGroup/MDSCPUDataSet' : {
+                'shape': (num_timesteps+1,),
+                'chunks': True,
+                'compression': 'gzip',
+                'dtype': 'f8',
+            },
+            '/MDSOpsGroup/MDSOpsDataSet' : {
+                'shape': (num_mds_ops,num_timesteps+1),
+                'chunks': True,
+                'compression': 'gzip',
+                'dtype': 'f8',
+            },
+            '/OSSCPUGroup/OSSCPUDataSet' : {
+                'shape': (num_osses, num_timesteps+1),
+                'chunks': True,
+                'compression': 'gzip',
+                'dtype': 'f8',
+            },
+            '/OSTReadGroup/OSTBulkReadDataSet' : {
+                'shape': (num_osts, num_timesteps+1),
+                'chunks': True,
+                'compression': 'gzip',
+                'dtype': 'f8',
+            },
+            '/OSTWriteGroup/OSTBulkWriteDataSet' : {
+                'shape': (num_osts, num_timesteps+1),
+                'chunks': True,
+                'compression': 'gzip',
+                'dtype': 'f8',
+            },
+        }
 
-        ### version 1 format
-        for x in [ 'OSTReadGroup/OSTBulkReadDataSet', 'OSTWriteGroup/OSTBulkWriteDataSet' ]:
-            if x not in self:
+        ### version 1 format - datasets
+        for dset_name, dset_params in _V1_SCHEMA.iteritems():
+            if dset_name not in self:
+                hard_name = '/version1/' + dset_name
                 self.create_dataset(
-                    name=x,
-                    shape=(num_osts, num_timesteps),
-                    chunks=True,
-                    compression="gzip",
-                    dtype='f8'
-                )
-        for x in [ 'FSStepsGroup/FSStepsDataSet' ]:
-            if x not in self:
-                self.create_dataset(
-                    name=x,
-                    shape=(num_timesteps,),
-                    chunks=True,
-                    compression="gzip",
-                    dtype='i8'
-                )
+                    name=hard_name,
+                    **dset_params)
+                self[dset_name] = h5py.SoftLink(hard_name)
+                tokio._debug_print( "creating softlink %s -> %s" % ( dset_name, hard_name ) )
 
-        self['FSStepsGroup/FSStepsDataSet'].attrs['fs'] = filesystem
-        self['FSStepsGroup/FSStepsDataSet'].attrs['host'] = host
+        ### version 1 format - metadata
+        self['/FSStepsGroup/FSStepsDataSet'].attrs['fs'] = filesystem
+        self['/FSStepsGroup/FSStepsDataSet'].attrs['host'] = host
+        self['/FSStepsGroup/FSStepsDataSet'].attrs['day'] = ""
+        self['/FSStepsGroup/FSStepsDataSet'].attrs['nextday'] = ""
+        self['/MDSOpsGroup/MDSOpsDataSet'].attrs['OpNames'] = mds_op_names
+        self['/OSSCPUGroup/OSSCPUDataSet'].attrs['OSSNames'] = oss_names
+        self['/OSTReadGroup/OSTBulkReadDataSet'].attrs['OSTNames'] = ost_names
+        self['/OSTWriteGroup/OSTBulkWriteDataSet'].attrs['OSTNames'] = ost_names
 
         ### version 2 format
         for x in [ 'ost_bytes_read', 'ost_bytes_written' ]:
@@ -78,14 +129,15 @@ class HDF5(h5py.File):
         ts_ct = int( (t_stop - t_start).total_seconds() / timestep )
 
         ### version 1 format - create a full dataset of timestamps
-        ts_map = np.empty( shape=(ts_ct,), dtype='i8' )
+        ts_map = np.empty( shape=(ts_ct+1,), dtype='i8' )
         for t in range( ts_ct ):
             ts_map[t] = t0_epoch + t * timestep
-        self['FSStepsGroup/FSStepsDataSet'][:] = ts_map[:]
+        ts_map[ts_ct] = t0_epoch + ts_ct * timestep # for the final extraneous v1 point...
+        self['/FSStepsGroup/FSStepsDataSet'][:] = ts_map[:]
         del ts_map ### free the array from memory
 
-        self['FSStepsGroup/FSStepsDataSet'].attrs['day'] = t_start.strftime("%Y-%m-%d")
-        self['FSStepsGroup/FSStepsDataSet'].attrs['nextday'] = (t0_day + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+        self['/FSStepsGroup/FSStepsDataSet'].attrs['day'] = t_start.strftime("%Y-%m-%d")
+        self['/FSStepsGroup/FSStepsDataSet'].attrs['nextday'] = (t0_day + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
 
         ### version 2 format - just store the first timestamp and the timestep
         self.first_timestamp = int(t0_epoch)
