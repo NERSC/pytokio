61a352a3020be5ada4eebef6d70522911e16fa0e
Glenn K. Lockwood <glock@lbl.gov>
2016-12-20 14:47:12 -0800

    added the repacker from fbench-ior2hdf5 to convert h5lmt into more portable hdf5 files

--- tokio/tools.py
+++ tokio/tools.py
@@ -6,12 +6,17 @@ have a stable API; as different functions prove themselves here, they will be
 incorporated into the formal TOKIO package API.
 """
 
-import os
 import tokio
-import time
+
+import os
+
 import datetime
 import h5py
-import numpy as np
+
+### for repack_h5lmt
+import subprocess
+import tempfile
+
 
 if os.environ.get("H5LMT_BASE"):
     H5LMT_BASE = os.environ.get("H5LMT_BASE")
@@ -20,7 +25,18 @@ else:
     H5LMT_BASE = "/global/project/projectdirs/pma/www/daily"
 
 def enumerate_h5lmts( file_name, datetime_start, datetime_end=None ):
+    """
+    Given a starting datetime and (optionally) an ending datetime, return all
+    H5LMT files that contain data inside of that date range (inclusive).
 
+    :param str file_name: The basename of the H5LMT files you want to find (e.g., cori_snx11168.h5lmt)
+    :param datetime_start: The first day of H5LMT data you want to collect
+    :type datetime_start: datetime.datetime
+    :param datetime_end: The last day of H5LMT data you want to collect
+    :type datetime_end: datetime.datetime or None
+    :return: fully qualified paths to existing H5LMT files (if datetime_end is not None), a single fully qualified path to an H5LMT file (if datetime_end is None), or None (if datetime_end is None)
+    :rtype: list(str) or str or None
+    """
     if datetime_end is None:
         datetime_end_local = datetime_start
     else:
@@ -42,6 +58,37 @@ def enumerate_h5lmts( file_name, datetime_start, datetime_end=None ):
             return h5lmt_files[0]
     else:
         return h5lmt_files
+
+def repack_h5lmt( src, dest, datasets ):
+    """
+    Take a source hdf5 file and a set of datasets and produce a dest hdf5 file
+    that contains only those datasets and that has been repacked.
+
+    :param str src: Path to input H5LMT file
+    :param str dest: Path to the intended output H5LMT file
+    :param datasets: Datasets to copy from src to dest
+    :type datasets: list(str)
+    :return: the last nonzero exit code
+    :rtype: int
+    """
+    if not os.path.isfile(src):
+        return -1
+
+    ret = 0
+    with tempfile.NamedTemporaryFile() as temp:
+        ### copy the relevant datasets
+        for dset in datasets:
+            cmd_args = ["h5copy", "-i", src, "-o", temp.name, "-s", dset, "-d", dset, "-p"]
+            ret_tmp = subprocess.call( cmd_args )
+            ### only copy nonzero exit codes into return value
+            if ret_tmp != 0: ret = ret_tmp
+
+        cmd_args = ["h5repack", "-L", "-v", "-f", "GZIP=1", temp.name, dest]
+        ret_tmp = subprocess.call( cmd_args )
+        ### only copy nonzero exit codes into return value
+        if ret_tmp != 0: ret = ret_tmp
+
+    return ret
 
 if __name__ == '__main__':
     pass
