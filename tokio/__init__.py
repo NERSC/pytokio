939671b0133a61b02c287f3be185d09d1c80090e
Glenn K. Lockwood <glock@lbl.gov>
2017-05-02 22:45:06 -0700

    don't fail to load tokio if HDF5/MySQLdb aren't available

--- tokio/__init__.py
+++ tokio/__init__.py
@@ -3,7 +3,14 @@
 TODO: add support for external configuration
 """
 
-from .connectors import HDF5, LMTDB
+try:
+    from .connectors.hdf5 import HDF5
+except ImportError:
+    pass
+try:
+    from .connectors.lmtdb import LMTDB
+except ImportError:
+    pass
 
 from .config import *
 from .debug import *
