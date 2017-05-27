1a4ec69ecac059f8b5b9eee362a9031f3501a7cc
Glenn K. Lockwood <glock@lbl.gov>
2017-05-26 22:59:22 -0700

    working craysdb module

--- test/test_connectors_craysdb.py
+++ test/test_connectors_craysdb.py
@@ -5,36 +5,27 @@ import json
 import tokio.connectors.craysdb
 
 SAMPLE_XTDB2PROC_FILE = os.path.join(os.getcwd(), 'inputs', 'sample.xtdb2proc')
-SAMPLE_XTPROCADMIN_FILE = os.path.join(os.getcwd(), 'inputs', 'sample.xtprocadmin')
+
+def verify_craysdbproc(craysdbproc):
+    for nidnum, record in craysdbproc.iteritems():
+        assert nidnum == record['processor_id']
+        for key in 'cab_position', 'cab_row', 'cage', 'cpu', 'process_slots', \
+                    'slot', 'x_coord', 'y_coord', 'z_coord':
+            assert record[key] >= 0
+        assert record['process_slots_free'] >= record['process_slots']
 
 def test_craysdbproc_from_cache():
     craysdbproc = tokio.connectors.craysdb.CraySDBProc( SAMPLE_XTDB2PROC_FILE )
-    print type(craysdbproc)
-    print craysdbproc.keys()
-    print json.dumps(craysdbproc, indent=4)
+    verify_craysdbproc(craysdbproc)
 
-# {
-#     "1": {
-#         "alloc_mode": "'interactive'", 
-#         "cab_position": "0", 
-#         "cab_row": "0", 
-#         "cabinet": "null", 
-#         "cage": "0", 
-#         "cpu": "1", 
-#         "next_red_black_switch": "null", 
-#         "od_allocator_id": "0", 
-#         "process_slots": "4", 
-#         "process_slots_free": "4", 
-#         "processor_id": "1", 
-#         "processor_spec": "null\n", 
-#         "processor_status": "'up'", 
-#         "processor_type": "'service'", 
-#         "slot": "0", 
-#         "x_coord": "0", 
-#         "y_coord": "0", 
-#         "z_coord": "0"
-#     }
-# }
+    try:
+        craysdbproc = tokio.connectors.craysdb.CraySDBProc()
+    except OSError as exception:
+        if exception.errno == 2:
+            ### sdb isn't available
+            pass
+    else:
+        verify_craysdbproc(craysdbproc)
 
 if __name__ == "__main__":
     test_craysdbproc_from_cache()
--- tokio/connectors/craysdb.py
+++ tokio/connectors/craysdb.py
@@ -6,6 +6,7 @@ node's configuration within the network fabric.
 
 import os
 import sys
+import collections
 import subprocess
 
 class CraySDBProc(dict):
@@ -17,35 +18,32 @@ class CraySDBProc(dict):
     This may someday become a base class for table-specific classes.
     """
     def __init__(self, cache_file=None):
-        dict.__init__(self)
+        super(CraySDBProc, self).__init__(self)
         self.cache_file = cache_file
-        self.dbdata = None
+        self.load_cache()
 
-    def __getitem__(self, key):
-        """
-        Lazy load the data but otherwise behave as a dict
-        """
-        if self.dbdata is None:
-            self.load_sdb_data()
-        return dict.__getitem__(self, key)
+#   def __getitem__(self, key):
+#       """
+#       Lazy load the data but otherwise behave as a dict
+#       """
+#       try:
+#           val = super(CraySDBProc, self).__getitem__(key)
+#       except KeyError:
+#           ### stubbing this out in case we want to lazy load keys later on
+#           raise
+#       else:
+#           return val
 
     def load_cache(self):
         """
         Load an xtdb2proc output file for a system
         """
-        if cache_file is None:
-            print "Loading from xtdb2proc"
-            try:
-                sdb = subprocess.check_output(['xtdb2proc', '-f', '-'])
-            except CalledProcessError as exception:
-                sys.stderr.write(exception.output + "\n")
-                raise
-            else:
-                self._load_cache(sdb)
+        if self.cache_file is None:
+            sdb = subprocess.check_output(['xtdb2proc', '-f', '-'])
+#           sdb = subprocess.Popen(['xtdb2proc', '-f', '-'], stdout=subprocess.PIPE).communicate()[0]
+            self._load_cache(sdb.splitlines())
         else:
-            print "Loading from xtdb2proc"
-            print "Loading from cache"
-            with open(cache_file, 'r') as fp:
+            with open(self.cache_file, 'r') as fp:
                 self._load_cache(fp)
 
     def _load_cache(self, iterable):
@@ -55,10 +53,17 @@ class CraySDBProc(dict):
         for line in iterable:
             if line.startswith('#'):
                 continue
+            elif line.strip() == "":
+                continue
             fields = line.split(',')
             record = {}
             for field in fields:
-                key, val = field.split('=', 1)
+                try:
+                    key, val = field.split('=', 1)
+                except:
+                    print line
+                    print field
+                    raise
                 ### remove extra quotes
                 val = val.strip().strip('\'"')
                 ### replace "null" with Python None values
@@ -72,7 +77,6 @@ class CraySDBProc(dict):
                         pass
                 record[key] = val
             key = int(record['processor_id'])
-            print "inserting key", key
             assert 'processor_id' in record
             assert key not in self
             self.__setitem__(key, record)
