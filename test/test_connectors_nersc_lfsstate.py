6cbcff2ee143b6887ccd4f4d787f3895f68d8e4f
Glenn K. Lockwood <glock@lbl.gov>
2017-06-16 23:31:45 -0700

    added the ost fullness parts to the nersc_lfsstate connector

--- test/test_connectors_nersc_lfsstate.py
+++ test/test_connectors_nersc_lfsstate.py
@@ -6,6 +6,7 @@ import tempfile
 import tokio.connectors.nersc_lfsstate
 
 SAMPLE_OSTMAP_FILE = os.path.join(os.getcwd(), 'inputs', 'sample_ost-map.txt')
+SAMPLE_OSTFULLNESS_FILE = os.path.join(os.getcwd(), 'inputs', 'sample_ost-fullness.txt')
 
 def verify_ostmap(ostmap):
     assert len(ostmap) > 0
@@ -52,3 +53,53 @@ def test_ostmap_serializer():
 
     ### verify the integrity of what we serialized
     verify_ostmap(ostmap)
+
+def test_ostfullness_from_cache():
+    ### read from a cache file
+    ostfullness = tokio.connectors.nersc_lfsstate.NERSCLFSOSTFullness( SAMPLE_OSTFULLNESS_FILE )
+
+    ### verify that the results are sensible
+#   verify_ostfullness(ostfullness)
+
+def test_ostfullness_serializer():
+    ### read from a cache file
+    ostfullness = tokio.connectors.nersc_lfsstate.NERSCLFSOSTFullness( SAMPLE_OSTFULLNESS_FILE )
+
+    ### serialize the object, then re-read it and verify it
+    cache_file = tempfile.NamedTemporaryFile(delete=False)
+    print "Caching to %s" % cache_file.name
+    ostfullness.save_cache(cache_file.name)
+
+    ### open a second file handle to this cached file to load it
+    ostfullness = tokio.connectors.nersc_lfsstate.NERSCLFSOSTFullness( cache_file.name )
+
+    ### destroy the cache file
+    cache_file.close()
+
+    ### verify the integrity of what we serialized
+#   verify_ostfullness(ostmap)
+
+def verify_ostfullness(ostfullness):
+    """
+    snx11035-OST0000_UUID 90767651352 66209262076 23598372720  74% /scratch2[OST:0]
+    """
+    assert len(ostfullness) > 0
+    print "Found %d time stamps" % len(ostfullness)
+    for timestamp, fs_data in ostfullness.iteritems():
+        assert len(fs_data) > 0
+        print "Found %d file systems" % len(fs_data)
+        for target_name, obd_data in fs_data.iteritems():
+            assert len(obd_data) > 0
+            print "Found %d OBD IDs" % len(obd_data)
+            obd_data = fs_data[target_name]
+            found_roles = set([])
+            for obd_name, keyvalues in obd_data.iteritems():
+                ### indices should never be negative
+                assert keyvalues['target_index'] >= 0
+                ### OBDs should never have no capacity
+                assert keyvalues['total_kib'] > 0
+                ### the sum of available and used should never exceed total
+                assert keyvalues['total_kib'] >= (keyvalues['remaining_kib'] + keyvalues['used_kib'])
+                found_roles.add(keyvalues['role'])
+            ### every Lustre file system should have at least one OSC
+            assert 'ost' in found_roles
--- tokio/connectors/nersc_lfsstate.py
+++ tokio/connectors/nersc_lfsstate.py
@@ -16,7 +16,13 @@ import os
 import sys
 import re
 
+### only try to match osc/mdc lines; skip mgc/lov/lmv
+# 351 UP osc snx11025-OST0007-osc-ffff8875ac1e7c00 3f30f170-90e6-b332-b141-a6d4a94a1829 5 10.100.100.12@o2ib1
 _REX_OST_MAP = re.compile('^\s*(\d+)\s+(\S+)\s+(\S+)\s+(snx\d+-\S+)\s+(\S+)\s+(\d+)\s+(\S+@\S+)\s*$')
+# snx11035-OST0000_UUID 90767651352 54512631228 35277748388  61% /scratch2[OST:0]
+#                             snx000-OST... tot     use     avail      00%    /scra[OST    :0     ]
+_REX_LFS_DF = re.compile('^\s*(snx\d+-\S+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+).\s+(\S+)\[([^:]+):(\d+)\]\s*$')
+
 
 class NERSCLFSOSTMap(dict):
     """
@@ -27,7 +33,6 @@ class NERSCLFSOSTMap(dict):
     This is a generally logical structure, although this map is always almost
     fed into a routine that tries to find multiple OSTs on the same OSS (i.e., a
     failover situation)
-
     """
     def __init__(self, cache_file=None):
         super(NERSCLFSOSTMap, self).__init__(self)
@@ -96,6 +101,94 @@ class NERSCLFSOSTMap(dict):
     def save_cache(self, output_file=None):
         """
         Serialize the object in a form compatible with the output of ost-map.txt
+        """
+        if output_file is None:
+            self._save_cache(sys.stdout)
+        else:
+            with open(output_file, 'w') as fp:
+                self._save_cache(fp)
+
+    def _save_cache(self, output):
+        output.write(str(self))
+
+class NERSCLFSOSTFullness(dict):
+    """
+    Parser for ost-fullness.txt.  Generates a dict of form
+
+        { timestamp(int) : { file_system: { ost_name : { keys: values } } } }
+    """
+    def __init__(self, cache_file=None):
+        super(NERSCLFSOSTFullness, self).__init__(self)
+        self.cache_file = cache_file
+        self.load_ost_fullness_file()
+
+    def __repr__(self):
+        """
+        snx11025-OST0001_UUID 90767651352 63381521692 26424604184  71% /scratch1[OST:1]
+        """
+        repr_result = ""
+        for timestamp in sorted(self.keys()):
+            repr_result += "BEGIN %d\n" % timestamp
+            fs_data = self[timestamp]
+            for target_name in sorted(fs_data.keys()):
+                obd_data = fs_data[target_name]
+                for obd_name in sorted(obd_data.keys(), key=lambda x: obd_data[x]['target_index']):
+                    keyvalues = obd_data[obd_name]
+                    repr_result += "%s-%s_UUID %ld %ld %ld %3d%% %s[%s:%d]\n" % (
+                        target_name,
+                        obd_name,
+                        keyvalues['total_kib'],
+                        keyvalues['used_kib'],
+                        keyvalues['remaining_kib'],
+                        ### note that lfs dl's percents are not divided by
+                        ### avail_kib, but rather the sum of used and remaining.
+                        ### unclear why these are different
+                        round(100.0 * keyvalues['used_kib'] / (keyvalues['remaining_kib'] + keyvalues['used_kib'])),
+                        keyvalues['mount_pt'],
+                        keyvalues['role'].upper(),
+                        keyvalues['target_index'], )
+
+        return repr_result
+
+    def load_ost_fullness_file(self):
+        """
+        Parse the output of a file containing concatenated outputs of `lfs df`
+        separated by lines of the form `BEGIN 0000` where 0000 is the UNIX epoch
+        time.  At NERSC, this file was (foolishly) called osts.txt in the h5lmt
+        dump directory.
+        """
+        this_timestamp = None
+        with open(self.cache_file, 'r') as fp:
+            for line in fp:
+                if line.startswith('BEGIN'):
+                    this_timestamp = int(line.split()[1])
+                    assert this_timestamp not in self
+                    self[this_timestamp] = {}
+                else:
+                    match = _REX_LFS_DF.search(line)
+                    if match is not None:
+                        file_system, target_name = re.findall('[^-_]+', match.group(1))[0:2]
+    
+                        if file_system not in self[this_timestamp]:
+                            self[this_timestamp][file_system] = {}
+    
+                        ### duplicates can happen if a file system is doubly mounted
+                        if target_name in self[this_timestamp][file_system]:
+                            raise KeyError("%s already found in timestamp %d" % (target_name, this_timestamp))
+    
+                        self[this_timestamp][file_system][target_name] = {
+                            'total_kib': long(match.group(2)),
+                            'used_kib': long(match.group(3)),
+                            'remaining_kib': long(match.group(4)),
+                            'mount_pt': match.group(6),
+                            'role': match.group(7).lower(),
+                            'target_index': int(match.group(8)),
+                        }
+
+    def save_cache(self, output_file=None):
+        """
+        Serialize the object in a form compatible with the output of
+        ost-fullness.txt
         """
         if output_file is None:
             self._save_cache(sys.stdout)
