4b036d489b2cf7a6930ad399a708de21e1a703f5
amahtout <mahtout.ahcene@gmail.com>
2017-06-27 14:47:12 -0700

    Rearranged test_connectors_nersc_lfstate

--- tests/test_connectors_hdf5.py
+++ tests/test_connectors_hdf5.py
@@ -16,35 +16,36 @@ POSITIVE_2D = [
     'OSTReadGroup/OSTBulkReadDataSet','OSTWriteGroup/OSTBulkWriteDataSet'
 ]
 
-file = tokio.connectors.HDF5(SAMPLE_INPUT)
+def test_connectors_hdf5():
+    file = tokio.connectors.HDF5(SAMPLE_INPUT)
 
-# Make sure group_name=None works
-assert len(file.to_dataframe().index)
+    # Make sure group_name=None works
+    assert len(file.to_dataframe().index)
+    
+    for dataset in DATASETS_1D:
+        assert dataset in file
+        assert len(file[dataset].shape) == 1
+        assert file[dataset][:].sum() > 0
+        assert len(file.to_dataframe(dataset).index)
+    
+    for dataset in DATASETS_2D:
+        assert dataset in file
+        assert len(file[dataset].shape) == 2
+        assert file[dataset][:,:].sum() > 0
+        assert len(file.to_dataframe(dataset).columns)
 
-for dataset in DATASETS_1D:
-    assert dataset in file
-    assert len(file[dataset].shape) == 1
-    assert file[dataset][:].sum() > 0
-    assert len(file.to_dataframe(dataset).index)
+        # Test dataset-dependent correctness
+        #
+        # Last timstamp greater than the first timestamp
+        assert file['FSStepsGroup/FSStepsDataSet'][0] < file['FSStepsGroup/FSStepsDataSet'][-1]
 
-for dataset in DATASETS_2D:
-    assert dataset in file
-    assert len(file[dataset].shape) == 2
-    assert file[dataset][:,:].sum() > 0
-    assert len(file.to_dataframe(dataset).columns)
-
-# Test dataset-dependent correctness
-#
-# Last timstamp greater than the first timestamp
-assert file['FSStepsGroup/FSStepsDataSet'][0] < file['FSStepsGroup/FSStepsDataSet'][-1]
-
-# No negative loads
-assert np.greater_equal(file['MDSCPUGroup/MDSCPUDataSet'][:], 0.0).all()
-
-# Only 0 or 1 allowed
-assert np.logical_or(np.equal(file['FSMissingGroup/FSMissingDataSet'][:,:], 0),
-                     np.equal(file['FSMissingGroup/FSMissingDataSet'][:,:], 1)).all()
-
-# No negative rates
-for dataset in POSITIVE_2D:
-    assert np.greater_equal(file[dataset][:], 0.0).all()
+        # No negative loads
+        assert np.greater_equal(file['MDSCPUGroup/MDSCPUDataSet'][:], 0.0).all()
+        
+        # Only 0 or 1 allowed
+        assert np.logical_or(np.equal(file['FSMissingGroup/FSMissingDataSet'][:,:], 0),
+                             np.equal(file['FSMissingGroup/FSMissingDataSet'][:,:], 1)).all()
+        
+        # No negative rates
+        for dataset in POSITIVE_2D:
+            assert np.greater_equal(file[dataset][:], 0.0).all()
--- tests/test_connectors_nersc_lfsstate.py
+++ tests/test_connectors_nersc_lfsstate.py
@@ -5,101 +5,84 @@ import json
 import tempfile
 import tokio.connectors.nersc_lfsstate
 
-SAMPLE_OSTMAP_FILE = os.path.join(os.getcwd(), 'inputs', 'sample_ost-map.txt')
-SAMPLE_OSTFULLNESS_FILE = os.path.join(os.getcwd(), 'inputs', 'sample_ost-fullness.txt')
+INPUTS = os.path.join(os.getcwd(), 'inputs')
+SAMPLE_OSTMAP_FILE = os.path.join(INPUTS, 'sample_ost-map.txt')
+SAMPLE_OSTFULLNESS_FILE = os.path.join(INPUTS, 'sample_ost-fullness.txt')
 
-def verify_ostmap(ostmap):
-    assert len(ostmap) > 0
-    print "Found %d time stamps" % len(ostmap)
-    for timestamp, fs_data in ostmap.iteritems():
-        assert len(fs_data) > 0
+def verify_ost(ost,type):
+    assert ost
+    print "Found %d time stamps" % len(ost)
+    
+    if type == 'ostmap': 
+        tmp_os = 'osc'
+    elif type == 'ostfullness':
+        tmp_os = 'ost'
+        
+    for timestamp, fs_data in ost.iteritems():
+        assert fs_data 
         print "Found %d file systems" % len(fs_data)
         for target_name, obd_data in fs_data.iteritems():
-            assert len(obd_data) > 0
+            assert obd_data
             print "Found %d OBD IDs" % len(obd_data)
             obd_data = fs_data[target_name]
-            found_roles = set([])
+            found_roles = set()
             for obd_name, keyvalues in obd_data.iteritems():
-                ### indices should never be negative
-                assert keyvalues['index'] >= 0
-                ### make sure that the role_id is consistent with parsed values
-                assert keyvalues['role_id'].startswith(target_name)
-                assert obd_name in keyvalues['role_id']
-                found_roles.add(keyvalues['role'])
-            ### every Lustre file system should have at least one OSC
-            assert 'osc' in found_roles
+                if type == 'ostmap': 
+                    verify_ostmap(obd_name, keyvalues, target_name)
+                elif type == 'ostfullness':
+                    verify_ostfullness(keyvalues)
 
-def test_ostmap_from_cache():
-    ### read from a cache file
-    ostmap = tokio.connectors.nersc_lfsstate.NERSCLFSOSTMap( SAMPLE_OSTMAP_FILE )
+                found_roles.add(unicode(keyvalues['role']))
+        # Every Lustre file system should have at least one OSC
+        assert tmp_os in found_roles
 
-    ### verify that the results are sensible
-    verify_ostmap(ostmap)
-
-def test_ostmap_serializer():
-    ### read from a cache file
-    ostmap = tokio.connectors.nersc_lfsstate.NERSCLFSOSTMap( SAMPLE_OSTMAP_FILE )
-
-    ### serialize the object, then re-read it and verify it
-    cache_file = tempfile.NamedTemporaryFile(delete=False)
-    print "Caching to %s" % cache_file.name
-    ostmap.save_cache(cache_file.name)
-
-    ### open a second file handle to this cached file to load it
-    ostmap = tokio.connectors.nersc_lfsstate.NERSCLFSOSTMap( cache_file.name )
-
-    ### destroy the cache file
-    cache_file.close()
-
-    ### verify the integrity of what we serialized
-    verify_ostmap(ostmap)
-
-def test_ostfullness_from_cache():
-    ### read from a cache file
-    ostfullness = tokio.connectors.nersc_lfsstate.NERSCLFSOSTFullness( SAMPLE_OSTFULLNESS_FILE )
-
-    ### verify that the results are sensible
-    verify_ostfullness(ostfullness)
-
-def test_ostfullness_serializer():
-    ### read from a cache file
-    ostfullness = tokio.connectors.nersc_lfsstate.NERSCLFSOSTFullness( SAMPLE_OSTFULLNESS_FILE )
-
-    ### serialize the object, then re-read it and verify it
-    cache_file = tempfile.NamedTemporaryFile(delete=False)
-    print "Caching to %s" % cache_file.name
-    ostfullness.save_cache(cache_file.name)
-
-    ### open a second file handle to this cached file to load it
-    ostfullness = tokio.connectors.nersc_lfsstate.NERSCLFSOSTFullness( cache_file.name )
-
-    ### destroy the cache file
-    cache_file.close()
-
-    ### verify the integrity of what we serialized
-    verify_ostfullness(ostfullness)
-
-def verify_ostfullness(ostfullness):
+def verify_ostmap(obd_name, keyvalues, target_name):
+    # Indices should never be negative
+    assert keyvalues['index'] >= 0
+    # Make sure that the role_id is consistent with parsed values
+    assert keyvalues['role_id'].startswith(target_name)
+    assert obd_name in keyvalues['role_id']
+    
+ 
+def verify_ostfullness(keyvalues):
     """
     snx11035-OST0000_UUID 90767651352 66209262076 23598372720  74% /scratch2[OST:0]
     """
-    assert len(ostfullness) > 0
-    print "Found %d time stamps" % len(ostfullness)
-    for timestamp, fs_data in ostfullness.iteritems():
-        assert len(fs_data) > 0
-        print "Found %d file systems" % len(fs_data)
-        for target_name, obd_data in fs_data.iteritems():
-            assert len(obd_data) > 0
-            print "Found %d OBD IDs" % len(obd_data)
-            obd_data = fs_data[target_name]
-            found_roles = set([])
-            for obd_name, keyvalues in obd_data.iteritems():
-                ### indices should never be negative
-                assert keyvalues['target_index'] >= 0
-                ### OBDs should never have no capacity
-                assert keyvalues['total_kib'] > 0
-                ### the sum of available and used should never exceed total
-                assert keyvalues['total_kib'] >= (keyvalues['remaining_kib'] + keyvalues['used_kib'])
-                found_roles.add(keyvalues['role'])
-            ### every Lustre file system should have at least one OSC
-            assert 'ost' in found_roles
+    assert keyvalues['target_index'] >= 0
+    assert keyvalues['total_kib'] > 0
+    assert keyvalues['total_kib'] >= (keyvalues['remaining_kib'] + keyvalues['used_kib'])
+             
+
+def test_ostmap_from_cache():
+    # Read from a cache file
+    ostmap = tokio.connectors.nersc_lfsstate.NERSCLFSOSTMap(SAMPLE_OSTMAP_FILE)
+    verify_ost(ostmap, type='ostmap')
+
+def test_ostfullness_from_cache():
+    # Read from a cache file
+    ostfullness = tokio.connectors.nersc_lfsstate.NERSCLFSOSTFullness(SAMPLE_OSTFULLNESS_FILE)
+    verify_ost(ostfullness, type='ostfullness')
+
+def test_ostmap_serializer():
+    # Read from a cache file
+    ostmap = tokio.connectors.nersc_lfsstate.NERSCLFSOSTMap(SAMPLE_OSTMAP_FILE)
+    # Serialize the object, then re-read it and verify it
+    cache_file = tempfile.NamedTemporaryFile(delete=False)
+    print "Caching to %s" % cache_file.name
+    ostmap.save_cache(cache_file.name)
+    # Open a second file handle to this cached file to load it
+    ostmap = tokio.connectors.nersc_lfsstate.NERSCLFSOSTMap(cache_file.name)
+    cache_file.close()
+    verify_ost(ostmap, type='ostmap')
+
+def test_ostfullness_serializer():
+    # Read from a cache file
+    ostfullness = tokio.connectors.nersc_lfsstate.NERSCLFSOSTFullness(SAMPLE_OSTFULLNESS_FILE)
+    # Serialize the object, then re-read it and verify it
+    cache_file = tempfile.NamedTemporaryFile(delete=False)
+    print "Caching to %s" % cache_file.name
+    ostfullness.save_cache(cache_file.name)
+    # Open a second file handle to this cached file to load it
+    ostfullness = tokio.connectors.nersc_lfsstate.NERSCLFSOSTFullness(cache_file.name)
+    cache_file.close()
+    verify_ost(ostfullness, type='ostfullness')
