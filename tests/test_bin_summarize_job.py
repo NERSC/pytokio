9591b9eb614c15113b87c752deb1f895970d8da6
Glenn K. Lockwood <glock@lbl.gov>
2017-07-14 17:37:22 -0700

    changed the input samples to provide enough consistent data for summarize_job to pass

--- tests/test_bin_summarize_job.py
+++ tests/test_bin_summarize_job.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import json
 import subprocess
 
 ### For tests that base all tests off of the sample Darshan log
@@ -20,42 +21,75 @@ SAMPLE_OSTFULLNESS_FILE = os.path.join(os.getcwd(), 'inputs', 'sample_ost-fullne
 
 BINARY = os.path.join('..', 'bin', 'summarize_job.py')
 
+def verify_output(output_str, key=None, value=None):
+    """
+    Given the stdout of summarize_job.py --json, ensure that it is valid json,
+    and ensure that a given key is present
+    """
+    for parsed_data in json.loads(output_str):
+        if key is not None:
+            assert key in parsed_data.keys()
+            if value is not None:
+                assert parsed_data[key] == value
+
+    return True
+
 def test_darshan_summary():
     """
-    Basic summarize_job.py functionality (darshan and LMT integration)
+    Baseline integration of darshan and LMT data
 
     """
-    assert subprocess.check_call([ BINARY, '--json', SAMPLE_DARSHAN_LOG ], stdout=open(os.devnull, 'w')) == 0
+    p = subprocess.Popen([ BINARY, '--json', SAMPLE_DARSHAN_LOG ], stdout=subprocess.PIPE)
+    output_str = p.communicate()[0]
+    assert p.returncode == 0
+    assert verify_output(output_str, key='darshan_agg_perf_by_slowest_posix')
+    assert verify_output(output_str, key='lmt_tot_gibs_written')
 
-#def test_darshan_summary_with_craysdb():
-#    """
-#    requires either an SDB cache file or access to xtdb2proc
-#    requires access to NERSC jobs db (to map jobid to node list)
-#    """
-#    assert subprocess.check_call([ BINARY, '--craysdb', SAMPLE_XTDB2PROC_FILE, '--json', SAMPLE_DARSHAN_LOG ]) == 0
+def test_darshan_summary_with_craysdb():
+    """
+    Integration of CraySdb
+
+    requires either an SDB cache file or access to xtdb2proc
+    requires either access to Slurm or a Slurm job cache file (to map jobid to node list)
+
+    """
+    p = subprocess.Popen([ BINARY, '--craysdb', SAMPLE_XTDB2PROC_FILE, '--json', SAMPLE_DARSHAN_LOG ], stdout=subprocess.PIPE)
+    output_str = p.communicate()[0]
+    assert p.returncode == 0
+    assert verify_output(output_str, key='darshan_agg_perf_by_slowest_posix')
+    assert verify_output(output_str, key='lmt_tot_gibs_written')
+    assert verify_output(output_str, key='craysdb_job_max_radius')
 
 def test_darshan_summary_with_lfsstatus():
     """
-    Integration between lfsstatus and summarize_job.py
+    Integration of lfsstatus
 
     """
-    assert subprocess.check_call([ BINARY,
-                                   '--json',
-                                   '--ost',
-                                   '--ost-fullness', SAMPLE_OSTFULLNESS_FILE,
-                                   '--ost-map', SAMPLE_OSTMAP_FILE,
-                                   SAMPLE_DARSHAN_LOG ], stdout=open(os.devnull, 'w')) == 0
+    p = subprocess.Popen([ BINARY,
+                           '--json',
+                           '--ost',
+                           '--ost-fullness', SAMPLE_OSTFULLNESS_FILE,
+                           '--ost-map', SAMPLE_OSTMAP_FILE,
+                           SAMPLE_DARSHAN_LOG ], stdout=subprocess.PIPE)
+    output_str = p.communicate()[0]
+    print output_str
+    assert p.returncode == 0
+    assert verify_output(output_str, key='darshan_agg_perf_by_slowest_posix')
+    assert verify_output(output_str, key='lmt_tot_gibs_written')
 
 def test_summary_without_darshan():
     """
-    Functionality when no darshan log is present
+    LMT-only functionality when no darshan log is present
 
     """
     os.environ['PYTOKIO_H5LMT_BASE'] = os.path.join(os.getcwd(), 'inputs' )
-    assert subprocess.check_call([ BINARY,
-                                   '--json',
-                                   '--jobid', SAMPLE_DARSHAN_JOBID,
-                                   '--start-time', SAMPLE_DARSHAN_START_TIME,
-                                   '--end-time', SAMPLE_DARSHAN_END_TIME,
-                                   '--file-system', SAMPLE_DARSHAN_FILE_SYSTEM,
-                                   ], stdout=open(os.devnull, 'w')) == 0
+    p = subprocess.Popen([ BINARY,
+                           '--json',
+                           '--jobid', SAMPLE_DARSHAN_JOBID,
+                           '--start-time', SAMPLE_DARSHAN_START_TIME,
+                           '--end-time', SAMPLE_DARSHAN_END_TIME,
+                           '--file-system', SAMPLE_DARSHAN_FILE_SYSTEM,
+                           ], stdout=subprocess.PIPE)
+    output_str = p.communicate()[0]
+    assert p.returncode == 0
+    assert verify_output(output_str, key='lmt_tot_gibs_written')
