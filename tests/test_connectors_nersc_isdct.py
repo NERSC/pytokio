1ac87919afe5d568069fe85a6d9f9bf1a820317c
Glenn K. Lockwood <glock@lbl.gov>
2017-08-25 15:10:44 -0700

    working isdct connector and tests passing

--- tests/test_connectors_nersc_isdct.py
+++ tests/test_connectors_nersc_isdct.py
@@ -1,20 +1,64 @@
 #!/usr/bin/env python
 
 import os
+import gzip
+import tarfile
 import tempfile
 import json
+import shutil
 import tokio.connectors.nersc_isdct
 
 INPUT_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'inputs')
 SAMPLE_TGZ_INPUT = os.path.join(INPUT_DIR, 'sample_nersc_idsct.tgz')
+SAMPLE_TAR_INPUT = os.path.join(INPUT_DIR, 'sample_nersc_idsct.tar')
 SAMPLE_UNPACKED_INPUT = os.path.join(INPUT_DIR, 'sample_nersc_idsct_dir')
 SAMPLE_JSON_INPUT = os.path.join(INPUT_DIR, 'sample_nersc_idsct.json')
+SAMPLE_JSON_GZ_INPUT = os.path.join(INPUT_DIR, 'sample_nersc_idsct.json.gz')
+DEFAULT_INPUT = SAMPLE_TGZ_INPUT
+
+def untar(input_file):
+    cleanup_untar(input_file)
+    tar = tarfile.open(input_file)
+    tar.extractall(path=INPUT_DIR)
+    tar.close()
+
+def cleanup_untar(input_file):
+    tar = tarfile.open(input_file)
+    for member in tar.getmembers():
+        fq_name = os.path.join(INPUT_DIR, member.name)
+        if os.path.exists(fq_name) and fq_name.startswith(INPUT_DIR): # one final backstop
+            print "Removing", fq_name
+            if member.isdir():
+                shutil.rmtree(fq_name)
+            else:
+                os.unlink(fq_name)
+
+def gunzip(input_file, output_file):
+    """
+    To check support for both compressed and uncompressed data streams, create
+    an uncompressed version of an input file on the fly
+    """
+    try_unlink(output_file)
+    with gzip.open(input_file, 'rb') as f:
+        file_content = f.read()
+    with open(output_file, 'w+b') as f:
+        print "Creating %s" % output_file
+        f.write(file_content)
+
+def try_unlink(output_file):
+    """
+    Destroy a temporarily decompressed input file
+    """
+    if os.path.exists(output_file):
+        print "Destroying %s" % output_file
+        os.unlink(output_file)
 
 def validate_object(isdct_data):
     """
     Ensure that the NerscIsdct class is correctly generated and initalized
     """
-    assert True
+    assert isdct_data is not None
+    assert len(isdct_data) > 0
 
 def validate_dataframe(isdct_data):
     """
@@ -29,36 +73,57 @@ def test_tgz_input():
     isdct_data = tokio.connectors.nersc_isdct.NerscIsdct(SAMPLE_TGZ_INPUT)
     validate_object(isdct_data)
 
+def test_tar_input():
+    """
+    Load .tar input files (no compression)
+    """
+    gunzip(SAMPLE_TGZ_INPUT, SAMPLE_TAR_INPUT)
+    isdct_data = tokio.connectors.nersc_isdct.NerscIsdct(SAMPLE_TAR_INPUT)
+    try_unlink(SAMPLE_TAR_INPUT)
+    validate_object(isdct_data)
+
 def test_unpacked_input():
     """
     Load unpacked .tgz directories
     """
+    untar(SAMPLE_TGZ_INPUT)
     isdct_data = tokio.connectors.nersc_isdct.NerscIsdct(SAMPLE_UNPACKED_INPUT)
+    cleanup_untar(SAMPLE_TGZ_INPUT) 
+    validate_object(isdct_data)
+
+def test_json_gz_input():
+    """
+    Load compressed, serialized json
+    """
+    isdct_data = tokio.connectors.nersc_isdct.NerscIsdct(SAMPLE_JSON_GZ_INPUT)
     validate_object(isdct_data)
 
 def test_json_input():
     """
     Load serialized json
     """
+    gunzip(SAMPLE_JSON_GZ_INPUT, SAMPLE_JSON_INPUT)
     isdct_data = tokio.connectors.nersc_isdct.NerscIsdct(SAMPLE_JSON_INPUT)
+    try_unlink(SAMPLE_JSON_INPUT)
     validate_object(isdct_data)
 
-def test_as_dataframe():
+def test_to_dataframe():
     """
     convert NerscIsdct object into a DataFrame
     """
-    isdct_data = tokio.connectors.nersc_isdct.NerscIsdct(SAMPLE_JSON_INPUT)
-    isdct_df = isdct_data.as_dataframe()
+    isdct_data = tokio.connectors.nersc_isdct.NerscIsdct(DEFAULT_INPUT)
+    isdct_df = isdct_data.to_dataframe()
     validate_dataframe(isdct_df)
 
 def test_serializer():
     # Read from a cache file
-    isdct_data = tokio.connectors.nersc_isdct.NerscIsdct(SAMPLE_JSON_INPUT)
+    isdct_data = tokio.connectors.nersc_isdct.NerscIsdct(DEFAULT_INPUT)
     # Serialize the object, then re-read it and verify it
-    cache_file = tempfile.NamedTemporaryFile(delete=False)
+    cache_file = tempfile.NamedTemporaryFile(delete=False, suffix='.json')
     print "Caching to %s" % cache_file.name
     isdct_data.save_cache(cache_file.name)
     # Open a second file handle to this cached file to load it
     isdct_cached = tokio.connectors.nersc_isdct.NerscIsdct(cache_file.name)
+    os.unlink(cache_file.name)
     cache_file.close()
     validate_object(isdct_cached)
--- tokio/connectors/nersc_isdct.py
+++ tokio/connectors/nersc_isdct.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
+import os
+import re
 import json
+import gzip
+import pandas
+import tarfile
+import mimetypes
 
 class NerscIsdct(dict):
     def __init__(self, input_file):
@@ -9,20 +15,251 @@ class NerscIsdct(dict):
         self.load()
 
     def __repr__(self):
-        return json.dumps(self.values())
+#       return json.dumps(self.values())
+        return json.dumps(self)
 
     def load(self):
         """
         Infer the type of input we're receiving, dispatch the correct loading
         function, and populate keys/values
         """
-        raise NotImplementedError
+        if os.path.isdir(self.input_file):
+            self.load_directory()
+        else:
+            # encoding can be 'gzip' or None
+            mime_type, encoding = mimetypes.guess_type(self.input_file)
+
+            if mime_type == 'application/x-tar':
+                self.load_tarfile(encoding)
+            elif mime_type == 'application/json':
+                self.load_json(encoding)
+            else:
+                raise Exception("Unknown mime type %s" % mime_type)
+
+    def load_tarfile(self, encoding):
+        """
+        Load a tarred (and optionally compressed) collection of ISDCT outputs.
+        Assume that ISDCT output files each contain a single output from a
+        single invocation of the isdct tool, and outputs are grouped into
+        directories named according to their nid numbers (e.g.,
+        nid00984/somefile.txt).
+        """
+        parsed_counters_list = []
+        if encoding == 'gzip':
+            tar = tarfile.open(self.input_file, 'r:gz')
+        else:
+            tar = tarfile.open(self.input_file, 'r')
+
+        for member in tar.getmembers():
+            # only care about file members, not directories
+            if not member.isfile():
+                continue
+
+            # extract the file member into a memory buffer
+            file_obj = tar.extractfile(member)
+
+            # infer the node name from the member's path
+            nodename = self._decode_nersc_nid(member.name)
+
+            parsed_counters_list.append(
+                self.parse_counters_fileobj(file_obj, nodename))
+
+        merged_counters = self._merge_parsed_counters(parsed_counters_list)
+        for key, val in merged_counters.iteritems():
+            self.__setitem__(key, val)
+
+    def load_directory(self):
+        """
+        Functionally identical to load_tarfile(), but operates on the untarred
+        contents of the tarfile.
+        """
+        parsed_counters_list = []
+        for root, dirs, files in os.walk(self.input_file):
+            for file_name in files:
+                fq_file_name = os.path.join(root, file_name)
+                nodename = self._decode_nersc_nid(fq_file_name)
+                parsed_counters = self.parse_counters_fileobj(
+                    open(fq_file_name, 'r'),
+                    nodename=nodename)
+                parsed_counters_list.append(parsed_counters)
+
+        merged_counters = self._merge_parsed_counters(parsed_counters_list)
+        for key, val in merged_counters.iteritems():
+            self.__setitem__(key, val)
+
+    def load_json(self, encoding):
+        """
+        Load the serialized format of this object, encoded as a json dictionary
+        where counters are keyed by the device serial number.  The converse of
+        the save_cache() method below.
+        """
+        if encoding == 'gzip':
+            open_func = gzip.open
+        else:
+            open_func = open
+
+        for key, val in json.load(open_func(self.input_file, 'r')).iteritems():
+            self.__setitem__(key, val)
  
     def save_cache(self, output_file=None):
         """
-        Save the dictionary in a json file
+        Save the dictionary in a json file.  This output can be read back in using
+        load_json() above.
         """
-        raise NotImplementedError
+        if output_file is None:
+            self._save_cache(sys.stdout)
+        else:
+            with open(output_file, 'w') as fp:
+                self._save_cache(fp)
 
     def _save_cache(self, output):
         output.write(str(self))
+
+    def to_dataframe(self, only_numeric=False):
+        """
+        Express the object as a dataframe
+        """
+        df = pandas.DataFrame.from_dict(self, orient='index')
+        if only_numeric:
+            numeric_df = df.apply(pandas.to_numeric, errors='coerce')
+            numeric_keys = []
+            for i in sorted(df.keys()):
+                ### don't print counters that are non-numeric
+                if df[i].dtype != np.int64 and df[i].dtype != np.float64:
+                    continue
+
+                ### don't print counters that are the same for all devices. df.count > 1
+                ### because we can't tell how many rows are unique if there's only one
+                ### row
+                if df.shape[0] > 1 and len(df[i].unique()) == 1:
+                    continue
+
+                numeric_keys.append( i )
+
+            if 'NodeName' in df:
+                numeric_keys = [ 'NodeName' ] + numeric_keys
+            return df[numeric_keys]
+        else:
+            return df
+
+    def parse_counters_fileobj(self, fileobj, nodename=None):
+        """
+        Read the output of a file-like object which contains the output of a
+        single isdct command.  Understands the output of the following options:
+          isdct show -smart (SMART attributes)
+          isdct show -sensor (device health sensors)
+          isdct show -performance (device performance metrics)
+          isdct show -a (drive info)
+        Outputs a dict of dicts keyed by the device serial number.
+        """
+    
+        data = {}
+        device_sn = None
+        parse_mode = 0      # =0 for regular counters, >1 for SMART data
+        smart_buffer = {}
+        for line in fileobj.readlines():
+            line = line.strip()
+            if device_sn is None:
+                rex_match = re.search( '(Intel SSD|SMART Attributes|SMART and Health Information).*(CVF[^ ]+-\d+)', line )
+                if rex_match is not None:
+                    device_sn = rex_match.group(2)
+
+                    if nodename is not None:
+                        data['NodeName'] = nodename
+                    if rex_match.group(1) == "Intel SSD":
+                        parse_mode = 0
+                    elif rex_match.group(1) == "SMART Attributes" or rex_match.group(1) == "SMART and Health Information":
+                        parse_mode = 1
+                    else:
+                        raise Exception("Unknown counter file format")
+            elif parse_mode == 0 and ':' in line:
+                key, val = line.split(':')
+                data[key.strip()] = val.strip()
+            elif parse_mode > 0 and ':' in line:
+                key, val = line.split(':')
+                smart_buffer[key.strip()] = val.strip()
+            elif parse_mode > 0 and line.startswith('-') and line.endswith('-'):
+                for key, val in self._rekey_smart_buffer(smart_buffer).iteritems():
+                    data[key.strip()] = val
+                smart_buffer = { '_id' : line.split()[1] }
+        if parse_mode > 0: # flush the last SMART register
+            for key, val in self._rekey_smart_buffer(smart_buffer).iteritems():
+                data[key] = val
+
+        if device_sn is None:
+            warnings.warn("Couldn't find device sn in " + path)
+        else:
+            return { device_sn : data }
+
+    def _merge_parsed_counters(self, parsed_counters_list):
+        """
+        Receives a list of parsed ISDCT outputs as dicts.  The counters from
+        each record are aggregated based on the NVMe device serial number, with
+        redundant counters being overwritten.
+        """
+        all_data = {}
+        for parsed_counters in parsed_counters_list:
+            if parsed_counters is None:
+                warnings.warn("No valid counters found in " + f)
+                continue
+            elif len(parsed_counters.keys()) > 1:
+                raise Exception("Received multiple serial numbers from parse_dct_counters_file")
+            else:
+                device_sn = parsed_counters.keys()[0] 
+    
+            ### merge file's counter dict with any previous counters we've parsed
+            if device_sn not in all_data:
+                all_data[device_sn] = parsed_counters[device_sn]
+            else:
+                all_data[device_sn].update(parsed_counters[device_sn])
+    
+        ### attempt to figure out the type of each counter
+        for device_sn, counters in all_data.iteritems():
+            for counter, value in counters.iteritems():
+                new_value = None
+                ### first, handle counters that do not have an obvious way to cast
+                if counter in ("Temperature", "Thermal_Throttle_Status_ThrottleStatus"):
+                    value = value.split()[0]
+    
+                ### the order here is important, but hex that is not prefixed with
+                ### 0x may be misinterpreted as integers.  if such counters ever
+                ### surface, they must be explicitly cast above
+                for cast in ( long, float, lambda x: long(x,16) ):
+                    try:
+                        new_value = cast(value)
+                        break
+                    except ValueError:
+                        pass
+                if value == "True":
+                    new_value = True
+                elif value == "False":
+                    new_value = False
+                if new_value is not None:
+                    all_data[device_sn][counter] = new_value
+    
+        return all_data
+
+    def _decode_nersc_nid(self, path):
+        """
+        Given a path to some ISDCT output file, somehow figure out what the nid
+        name for that node is.  This encoding is specific to the way NERSC collects
+        and preserves ISDCT outputs.
+        """
+        abs_path = os.path.abspath(path)
+        nid_name = os.path.dirname(abs_path).split(os.sep)[-1]
+        return nid_name
+
+    def _rekey_smart_buffer(self, smart_buffer):
+        """
+        Take a buffer containing smart values associated with one register and
+        create unique counters
+        """
+        data = {}
+        prefix = smart_buffer.get("Description")
+        if prefix is None:
+            prefix = smart_buffer.get("_id")
+    
+        for key, val in smart_buffer.iteritems():
+            key = ("%s_%s" % (prefix, key.strip())).replace("-","").replace(" ", "_")
+            data[key] = val.strip()
+        return data
