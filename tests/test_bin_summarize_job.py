256bf044160dd8332c6725bb48d9811b47feda3d
Glenn K. Lockwood <glock@lbl.gov>
2017-09-15 21:40:42 -0700

    add a few more functionality tests

--- tests/test_bin_summarize_job.py
+++ tests/test_bin_summarize_job.py
@@ -2,10 +2,13 @@
 
 import os
 import json
+import pandas
+import StringIO
 import subprocess
 
 ### For tests that base all tests off of the sample Darshan log
 SAMPLE_DARSHAN_LOG = os.path.join(os.getcwd(), 'inputs', 'sample.darshan')
+SAMPLE_DARSHAN_LOG_2 = os.path.join(os.getcwd(), 'inputs', 'sample-2.darshan')
 
 ### For tests that function without the Darshan log--these values must reflect
 ### the contents of SAMPLE_DARSHAN_LOG for the tests to actually pass
@@ -21,7 +24,7 @@ SAMPLE_OSTFULLNESS_FILE = os.path.join(os.getcwd(), 'inputs', 'sample_ost-fullne
 
 BINARY = os.path.join('..', 'bin', 'summarize_job.py')
 
-def verify_output(output_str, key=None, value=None):
+def verify_output_json(output_str, key=None, value=None):
     """
     Given the stdout of summarize_job.py --json, ensure that it is valid json,
     and ensure that a given key is present
@@ -34,16 +37,71 @@ def verify_output(output_str, key=None, value=None):
 
     return True
 
-def test_darshan_summary():
+def verify_output_csv(output_str, key=None, value=None, expected_rows=None):
     """
-    Baseline integration of darshan and LMT data
+    Given the stdout of summarize_job.py, ensure that it is re-readable by
+    pandas.read_csv and ensure that a given column (key) is present
+    """
+    df = pandas.read_csv(StringIO.StringIO(output_str))
+    if key is not None:
+        assert key in df.columns
+
+    if value is not None:
+        assert df[key][0] == value
+
+    if expected_rows is not None:
+        assert len(df) == expected_rows
+
+    return True
+
+def test_darshan_summary_json():
+    """
+    Baseline integration of darshan and LMT data (json)
 
     """
     p = subprocess.Popen([ BINARY, '--json', SAMPLE_DARSHAN_LOG ], stdout=subprocess.PIPE)
     output_str = p.communicate()[0]
     assert p.returncode == 0
-    assert verify_output(output_str, key='darshan_agg_perf_by_slowest_posix')
-    assert verify_output(output_str, key='lmt_tot_gibs_written')
+    assert verify_output_json(output_str, key='darshan_agg_perf_by_slowest_posix')
+    assert verify_output_json(output_str, key='lmt_tot_gibs_written')
+
+def test_darshan_summary_csv():
+    """
+    Baseline integration of darshan and LMT data (csv)
+
+    """
+    p = subprocess.Popen([ BINARY, SAMPLE_DARSHAN_LOG ], stdout=subprocess.PIPE)
+    output_str = p.communicate()[0]
+    assert p.returncode == 0
+    assert verify_output_csv(output_str, key='darshan_agg_perf_by_slowest_posix')
+    assert verify_output_csv(output_str, key='lmt_tot_gibs_written')
+
+def test_darshan_summaries():
+    """
+    Correctly handle multiple Darshan logs (csv)
+
+    """
+    p = subprocess.Popen([ BINARY, SAMPLE_DARSHAN_LOG, SAMPLE_DARSHAN_LOG_2 ], stdout=subprocess.PIPE)
+    output_str = p.communicate()[0]
+    assert p.returncode == 0
+    assert verify_output_csv(output_str, key='darshan_agg_perf_by_slowest_posix', expected_rows=2)
+    assert verify_output_csv(output_str, key='lmt_tot_gibs_written', expected_rows=2)
+
+def test_bogus_darshans():
+    """
+    Correctly handle mix of valid and invalid Darshan logs
+
+    """
+    p = subprocess.Popen([ BINARY,
+                           SAMPLE_DARSHAN_LOG,      # valid log
+                           SAMPLE_XTDB2PROC_FILE,   # not valid log
+                           SAMPLE_DARSHAN_LOG_2,    # valid log
+                           'garbagefile'            # file doesn't exist
+                         ], stdout=subprocess.PIPE)
+    output_str = p.communicate()[0]
+    assert p.returncode == 0
+    assert verify_output_csv(output_str, key='darshan_agg_perf_by_slowest_posix', expected_rows=2)
+    assert verify_output_csv(output_str, key='lmt_tot_gibs_written', expected_rows=2)
 
 def test_darshan_summary_with_craysdb():
     """
@@ -56,9 +114,9 @@ def test_darshan_summary_with_craysdb():
     p = subprocess.Popen([ BINARY, '--craysdb', SAMPLE_XTDB2PROC_FILE, '--json', SAMPLE_DARSHAN_LOG ], stdout=subprocess.PIPE)
     output_str = p.communicate()[0]
     assert p.returncode == 0
-    assert verify_output(output_str, key='darshan_agg_perf_by_slowest_posix')
-    assert verify_output(output_str, key='lmt_tot_gibs_written')
-    assert verify_output(output_str, key='craysdb_job_max_radius')
+    assert verify_output_json(output_str, key='darshan_agg_perf_by_slowest_posix')
+    assert verify_output_json(output_str, key='lmt_tot_gibs_written')
+    assert verify_output_json(output_str, key='craysdb_job_max_radius')
 
 def test_darshan_summary_with_lfsstatus():
     """
@@ -74,8 +132,8 @@ def test_darshan_summary_with_lfsstatus():
     output_str = p.communicate()[0]
     print output_str
     assert p.returncode == 0
-    assert verify_output(output_str, key='darshan_agg_perf_by_slowest_posix')
-    assert verify_output(output_str, key='lmt_tot_gibs_written')
+    assert verify_output_json(output_str, key='darshan_agg_perf_by_slowest_posix')
+    assert verify_output_json(output_str, key='lmt_tot_gibs_written')
 
 def test_summary_without_darshan():
     """
@@ -92,4 +150,4 @@ def test_summary_without_darshan():
                            ], stdout=subprocess.PIPE)
     output_str = p.communicate()[0]
     assert p.returncode == 0
-    assert verify_output(output_str, key='lmt_tot_gibs_written')
+    assert verify_output_json(output_str, key='lmt_tot_gibs_written')
