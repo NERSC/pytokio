016e46318e4f2b7b2ca6cc40c5d2c3c4ae967c28
Glenn K. Lockwood <glennklockwood@gmail.com>
2017-09-23 14:35:00 -0700

    rough first cut at a CachingDb class

---
+++ tests/test_connectors_cachingdb.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+"""
+Tests for the CachingDb class and functionality.  Relies on a sample LMT
+database to exercise its baseline functionality; in most environments, this
+database is not available, so only the sqlite3 functionality will be exercised.
+"""
+
+import os
+import nose
+import tokiotest
+import tokio.connectors.cachingdb
+
+_DB_HOST = os.environ.get('TEST_DB_HOST', 'localhost')
+_DB_USER = os.environ.get('TEST_DB_USER', 'root')
+_DB_PASSWORD = os.environ.get('TEST_DB_PASSWORD', '')
+_DB_DBNAME = os.environ.get('TEST_DB_DBNAME', 'testdb')
+
+INPUT_CACHE_FILE = 'cache.sqlite3'
+
+### You must ensure that each TEST_TABLES table has at least
+### LIMIT_CYCLES[-1] records for this test.  This is why we stop
+### at 24 below (the sample input from LMT has a table with only 24 OST
+### records)
+LIMIT_CYCLES = [8, 16, 24]
+
+TEST_TABLES = [
+    (
+        'OSS_DATA',
+        'OSS_ID, TS_ID, PCT_CPU, PCT_MEMORY, PRIMARY KEY (OSS_ID, TS_ID)',
+    ),
+    (
+        'OSS_INFO',
+        'OSS_ID, FILESYSTEM_ID, HOSTNAME, FAILOVERHOST, PRIMARY KEY(OSS_ID, HOSTNAME)',
+    ),
+    (
+        'OST_DATA',
+        """OST_ID, TS_ID, READ_BYTES, WRITE_BYTES, PCT_CPU, KBYTES_FREE,
+           KBYTES_USED, INODES_FREE, INODES_USED, PRIMARY KEY(OST_ID, TS_ID)""",
+    ),
+    (
+        'OST_INFO',
+        'OST_ID, OSS_ID, OST_NAME, HOSTNAME, OFFLINE, DEVICE_NAME, PRIMARY KEY (OST_ID)',
+    ),
+]
+
+
+def query_without_saving(test_db):
+    """
+    cachingdb does not save results when table=None
+    """
+    for test_table, _ in TEST_TABLES:
+        limit = 10
+        result = test_db.query(
+            query_str='SELECT * from %s LIMIT %d' % (test_table, limit))
+        assert len(result) > 0
+        assert len(test_db.saved_results) == 0 # no saved results
+
+@nose.tools.with_setup(tokiotest.create_tempfile, tokiotest.delete_tempfile)
+def verify_cache_functionality(test_db):
+    """
+    Performs the following integration test:
+
+    1. get a limited number of records from each table in a database
+    2. ensure that all results are saved in memory
+    3. cache the entire db object to sqlite
+    4. ensure that all tables saved in memory were correctly written out
+    5. ensure that the degenerate results stored in memory are written out to
+       the cache db only once
+
+    Also verifies the following desired behavior:
+
+    1. save_cache works when table_schema is only provided once, and not on the
+       final query
+    """
+    for test_table, test_table_schema in TEST_TABLES:
+        sum_limits = 0
+        for iteration, limit in enumerate(LIMIT_CYCLES):
+            sum_limits += limit
+            if iteration == 0:
+                schema_param = 'CREATE TABLE IF NOT EXISTS %s (%s)' % (
+                    test_table,
+                    test_table_schema)
+            else:
+                schema_param = None
+            result = test_db.query(
+                query_str='SELECT * from %s LIMIT %d' % (test_table, limit),
+                table=test_table,
+                table_schema=schema_param)
+            print "Got %d records from a limit of %d in %s" % (
+                len(result),
+                limit,
+                test_table)
+            assert len(result) == limit
+            assert len(test_db.saved_results[test_table]['rows']) == sum_limits
+
+    ### Cache all TEST_TABLES to a single file
+    test_db.save_cache(tokiotest.TEMP_FILE.name)
+    print "Created", tokiotest.TEMP_FILE.name
+
+    ### Destroy any residual state, just in case
+    test_db.close()
+    del test_db
+
+    ### Open the cache database we just created
+    test_db = tokio.connectors.cachingdb.CachingDb(cache_file=tokiotest.TEMP_FILE.name)
+
+    ### Confirm that all tables were written
+    result = test_db.query("SELECT COUNT(name) from sqlite_master WHERE type='table'")
+    print "Found %d tables in %s" % (result[0][0], test_db.cache_file)
+    assert result[0][0] == len(TEST_TABLES)
+
+    ### Confirm that the size of each table is correct
+    for test_table, test_table_schema in TEST_TABLES:
+        result = test_db.query(
+            query_str='SELECT * from %s' % (test_table),
+            table=test_table,
+            table_schema=schema_param)
+        print "Found table %s with %d records" % (test_table, len(result))
+        assert len(result) == LIMIT_CYCLES[-1]
+
+TEST_FUNCTIONS = [
+    (
+        "cachingdb does not save results when table=None",
+        query_without_saving,
+    ),
+    (
+        "cachingdb.save_cache functionality",
+        verify_cache_functionality,
+    ),
+]
+
+def test_remote_db():
+    """
+    Verify functionality when connecting to a remote database
+    """
+    test_db = tokio.connectors.cachingdb.CachingDb(
+        dbhost=_DB_HOST,
+        dbuser=_DB_USER,
+        dbpassword=_DB_PASSWORD,
+        dbname=_DB_DBNAME)
+
+    for description, test_function in TEST_FUNCTIONS:
+        func = test_function
+        func.description = description
+        yield func, test_db
+
+def test_cache_db():
+    """
+    Verify functionality when connecting to a remote database
+    """
+    test_db = tokio.connectors.cachingdb.CachingDb(cache_file=INPUT_CACHE_FILE)
+
+    for description, test_function in TEST_FUNCTIONS:
+        func = test_function
+        func.description = description
+        ### TODO: get this working
+#       yield func, test_db
---
+++ tokio/connectors/cachingdb.py
@@ -0,0 +1,289 @@
+#!/usr/bin/env python
+"""
+Generic infrastructure for retrieving data from a relational database that
+contains immutable data.  Can use a local caching database (sqlite3) to allow
+for reanalysis on platforms that cannot access the original remote database.
+"""
+
+import warnings
+#try:
+#    import MySQLdb
+#except ImportError:
+import pymysql
+pymysql.install_as_MySQLdb()
+import MySQLdb
+#    pass
+import sqlite3
+
+CACHE_TABLE_SCHEMA = """
+create table if not exists
+    summary(stepid primary key, hostname, start integer, completion integer, numnodes integer)
+"""
+
+HIT_CACHE_DB = 1
+HIT_REMOTE_DB = 2
+
+class CachingDb(object):
+    """
+    Connect to and interact with a relational database. If this class is
+    instantiated with a cache_file argument, all queries will go to that
+    SQLite-based cache database.  If this class is not instantiated with a
+    cache_file argument, all queries will go out to the remote database.
+    """
+    #pylint: disable=too-many-arguments
+    def __init__(self, dbhost=None, dbuser=None, dbpassword=None, dbname=None, cache_file=None):
+        # self.saved_results is the in-memory data cache.  It has a structure of
+        # saved_results = {
+        #    'table1': {
+        #       'rows': [ (row1), (row2), (row3), ... ],
+        #       'schema': 'create table if not exists table1(...)',
+        #    }
+        #    'table2': { ... }
+        #    ...
+        # }
+        self.saved_results = {}
+        self.last_hit = None
+
+        # cache db
+        self.cache_file = None
+        self.cache_db = None
+        self.cache_db_ps = None
+
+        # actual db
+        self.remote_db = None
+        self.remote_db_ps = None
+
+        # Connect to cache db if specified
+        if cache_file is not None:
+            self.connect_cache(cache_file)
+            # TODO: check to ensure db isn't empty
+#           result = self.query('select * from summary limit 1', (), nocache=True)
+#           if len(result) == 0:
+#               warnings.warn("Using an empty cache database; queries will return nothing")
+
+        if dbhost is not None \
+        and dbuser is not None \
+        and dbpassword is not None \
+        and dbname is not None:
+            self.connect(dbhost=dbhost,
+                         dbuser=dbuser,
+                         dbpassword=dbpassword,
+                         dbname=dbname)
+
+    def connect(self, dbhost, dbuser, dbpassword, dbname):
+        """
+        Establish db connection
+        """
+        if self.cache_db is not None:
+            # can't really do both local and remote dbs; all queries will be
+            # run against the cache_db, which is probably not what someone
+            # wants
+            warnings.warn("attempting to use both remote and cache db; disabling cache db")
+            self.close_cache()
+
+        self.remote_db = MySQLdb.connect(host=dbhost,
+                                         user=dbuser,
+                                         passwd=dbpassword,
+                                         db=dbname)
+        self.remote_db_ps = get_paramstyle_symbol(MySQLdb.paramstyle)
+
+    def close(self):
+        """
+        Destroy connection objects and reset state of remote connection
+        attributes
+        """
+        self.remote_db = None
+        self.remote_db_ps = None
+
+    def connect_cache(self, cache_file):
+        """
+        Open the cache db and note whether our intent is to only write to it, or
+        read and write.
+        """
+        if cache_file is not None:
+            self.cache_db = sqlite3.connect(cache_file)
+            self.cache_file = cache_file
+            self.cache_db_ps = get_paramstyle_symbol(sqlite3.paramstyle)
+            ### TODO: Ensure that the correct table/schema exists
+#           self.cache_db.execute(CACHE_TABLE_SCHEMA)
+
+    def close_cache(self):
+        """
+        Close a cache db and reset its intent
+        """
+        if self.cache_db is not None:
+            self.cache_db = self.cache_db.close()
+            self.cache_file = None
+
+    def drop_cache(self, tables=None):
+        """
+        Flush saved results from memory.  If tables are specified, only drop
+        those tables' results.  If no tables are provided, flush everything.
+        """
+        drop_caches = set([])
+        for table in self.saved_results.keys():
+            if tables is None or table in tables:
+                drop_caches.add(table)
+
+        for drop_cache in drop_caches:
+            del self.saved_results[drop_cache]
+
+    def save_cache(self, cache_file):
+        """
+        Commit the in-memory cache to a cache database.  Currently very
+        memory-inefficient and not good for caching giant pieces of a database
+        without something wrapping it to feed it smaller pieces.
+
+        Also note that we manipulate the object's cache_db* attributes in a
+        dirty way here to prevent closing and re-opening the original cache
+        db.  If the self.open_cache() is ever changed to include tracking
+        more state, this function must also be updated to retain that state
+        while the old cache db state is being temporarily shuffled out.
+        """
+        ### Shuffle out the old cache db state (if it exists)
+        old_state = {}
+        if self.cache_file is not None and self.cache_file != cache_file:
+            old_state = {
+                'cache_file': self.cache_file,
+                'cache_db': self.cache_db,
+                'cache_db_ps': self.cache_db_ps,
+            }
+
+        ### Open a new cache db connection without closing the old cache db
+        self.connect_cache(cache_file)
+
+        # saved_results = {
+        #    'table1': {
+        #       'rows': [ (row1), (row2), (row3), ... ],
+        #       'schema': 'create table if not exists table1(...)',
+        #    }
+        #    'table2': { ... }
+        #    ...
+        # }
+
+        ### Commit each table we've retained in memory
+        drop_caches = set([])
+        for table, table_info in self.saved_results.iteritems():
+            num_fields = None
+            ### Verify and preprocess each saved row
+            for index, row in enumerate(self.saved_results[table]['rows']):
+                if num_fields is None:
+                    num_fields = len(row)
+
+                ### Verify that the rows we've saved are actually all of the
+                ### same length so that they have a hope of being inserted
+                ### into the schema
+                if len(row) != num_fields:
+                    warnings.warn(
+                        "saved_results[%s] contains non-uniform rows (%d, %d); skipping table" %
+                        table, len(row), num_fields)
+                    continue
+                else:
+                    ### Prepend table name to row to facilitate the bulk insert
+                    ### query below
+#                   self.saved_results[table]['rows'][index] = (table,) + row
+                    pass
+
+            ### Create the table (if necessary).  This will throw all sorts of
+            ### exceptions if
+            ###   (1) the table doesn't already exist in the cache database, or
+            ###   (2) 'schema' isn't set correctly by the downstream application
+            if table_info['schema'] is not None:
+                self.cache_db.execute(table_info['schema'])
+
+            ### INSERT OR REPLACE so that the cache db never wins if a duplicate
+            ### primary key is detected
+            query_str = "insert or replace into %s values (%s)" % (
+                table,
+                ','.join(['?'] * num_fields))
+            self.cache_db.executemany(
+                query_str,
+                table_info['rows'])
+            self.cache_db.commit()
+
+            ### Drop committed rows from memory
+            drop_caches.add(table)
+
+        for drop_cache in drop_caches:
+            del self.saved_results[drop_cache]
+
+        self.close_cache()
+
+        ### Shuffle back in the state of the old cache db
+        if len(old_state) > 0:
+            self.cache_file = old_state['cache_file']
+            self.cache_db = old_state['old_cache_db']
+            self.cache_db_ps = old_state['old_cache_db_ps']
+
+    def query(self, query_str, table=None, table_schema=None, query_variables=()):
+        """
+        Pass a query through all layers of cache and return on the first hit.
+        If a table is specified, the results of this query can be saved to the
+        cache db into a table of that name.
+        """
+
+        ### Collapse query string to remove extraneous whitespace
+        query_str = ' '.join(query_str.split())
+
+        ### Check the cache database (if available)
+        if self.cache_db is not None:
+            results = self._query_sqlite3(query_str, query_variables)
+            self.last_hit = HIT_CACHE_DB
+        ### Check the MySQL database (if available)
+        elif self.remote_db is not None:
+            results = self._query_mysql(query_str, query_variables)
+            self.last_hit = HIT_REMOTE_DB
+        else:
+            raise RuntimeError('No databases available to query')
+
+        if table is not None:
+            ### Initialize the table if our intent is to save the result of this
+            ### query.
+            if table not in self.saved_results:
+                self.saved_results[table] = {
+                    'rows': [],
+                    'schema': None,
+                }
+            ### Table schema can be defined or re-defined on any query.  It is
+            ### up to the downstream application to manage this correctly.
+            if table_schema is not None:
+                self.saved_results[table]['schema'] = table_schema
+
+            ### Append our results
+            self.saved_results[table]['rows'] += list(results)
+
+        return results
+
+    def _query_sqlite3(self, query_str, query_variables):
+        """
+        Run a query against the cache database and yield the full output.  No
+        buffering, so be careful.
+        """
+        cursor = self.cache_db.cursor()
+        cursor.execute(query_str % {'ps': self.cache_db_ps}, query_variables)
+        rows = cursor.fetchall()
+        cursor.close()
+        return rows
+
+    def _query_mysql(self, query_str, query_variables):
+        """
+        Run a query against the MySQL database and yield the full output tuple.
+        No buffering, so be careful.
+
+        """
+        cursor = self.remote_db.cursor()
+        cursor.execute(query_str % {'ps': self.remote_db_ps}, query_variables)
+        rows = cursor.fetchall()
+        cursor.close()
+        return rows
+
+def get_paramstyle_symbol(paramstyle):
+    """
+    Infer the correct paramstyle for a database.paramstyle (see PEP-0249)
+    """
+    if paramstyle == 'qmark':
+        return "?"
+    elif paramstyle == 'format' or paramstyle == 'pyformat':
+        return "%s"
+    else:
+        raise Exception("Unsupported paramstyle %s" % paramstyle)
