b0a3328d1c264153389a02472d073638c60b91ec
Glenn K. Lockwood <glennklockwood@gmail.com>
2017-09-23 16:55:54 -0700

    converted NerscJobsDb to be a subclass of CachingDb

--- tokio/connectors/nersc_jobsdb.py
+++ tokio/connectors/nersc_jobsdb.py
@@ -16,11 +16,17 @@ a cache database.
 
 import os
 import warnings
+import sqlite3
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+except ImportError:
+    pass
 try:
     import MySQLdb
 except ImportError:
     pass
-import sqlite3
+import cachingdb
 
 CACHE_TABLE_SCHEMA = """
 create table if not exists
@@ -31,7 +37,7 @@ HIT_MEMORY   = 0
 HIT_CACHE_DB = 1
 HIT_REMOTE_DB = 2
 
-class NerscJobsDb(object):
+class NerscJobsDb(cachingdb.CachingDb):
     """
     Connect to and interact with the NERSC jobs database.  Maintains a query
     cache where the results of queries are cached in memory.  If a query is
@@ -53,117 +59,30 @@ class NerscJobsDb(object):
     def __init__(self, dbhost=None, dbuser=None, dbpassword=None, dbname=None, cache_file=None):
         # in-memory query cache
         self.cached_queries = {}
-        self.last_results = None
-        self.last_hit = None
+        self.last_results = None # for debugging
 
-        # cache db
-        self.cache_file = None
-        self.cache_db = None
-        self.cache_db_ps = None
+        if dbhost is None:
+            dbhost = os.environ.get('NERSC_JOBSDB_HOST')
+        if dbuser is None:
+            dbuser = os.environ.get('NERSC_JOBSDB_USER')
+        if dbpassword is None:
+            dbpassword = os.environ.get('NERSC_JOBSDB_PASSWORD')
+        if dbname is None:
+            dbname = os.environ.get('NERSC_JOBSDB_DB')
 
-        # actual db
-        self.remote_db = None
-        self.remote_db_ps = None
+        super(NerscJobsDb, self).__init__(
+            dbhost=dbhost,
+            dbuser=dbuser,
+            dbpassword=dbpassword,
+            dbname=dbname,
+            cache_file=cache_file)
 
-        # Connect to cache db if specified
-        if cache_file is not None:
-            self.connect_cache(cache_file)
-            # check to ensure db isn't empty
-            result = self.query('select * from summary limit 1', (), nocache=True)
-            if len(result) == 0:
-                warnings.warn("Using an empty cache database; queries will return nothing")
-
-        # Get database parameters only when not attempting to connect to a cache db
-        if self.cache_db is None:
-            if dbhost is None:
-                dbhost = os.environ.get('NERSC_JOBSDB_HOST')
-            if dbuser is None:
-                dbuser = os.environ.get('NERSC_JOBSDB_USER')
-            if dbpassword is None:
-                dbpassword = os.environ.get('NERSC_JOBSDB_PASSWORD')
-            if dbname is None:
-                dbname = os.environ.get('NERSC_JOBSDB_DB')
-
-        if dbhost is not None \
-        and dbuser is not None \
-        and dbpassword is not None \
-        and dbname is not None:
-            self.connect(dbhost=dbhost,
-                         dbuser=dbuser,
-                         dbpassword=dbpassword,
-                         dbname=dbname)
-
-    def connect(self, dbhost, dbuser, dbpassword, dbname):
-        """
-        Establish db connection
-        """
-        if self.cache_db is not None:
-            # can't really do both local and remote dbs; all queries will be
-            # run against the cache_db, which is probably not what someone
-            # wants
-            warnings.warn("attempting to use both remote and cache db; disabling cache db")
-            self.close_cache()
-
-        self.remote_db = MySQLdb.connect(host=dbhost,
-                                        user=dbuser,
-                                        passwd=dbpassword,
-                                        db=dbname)
-        self.remote_db_ps = get_paramstyle_symbol(MySQLdb.paramstyle)
-
-    def connect_cache(self, cache_file):
-        """
-        Open the cache db and note whether our intent is to only write to it, or
-        read and write.
-        """
-        if cache_file is not None:
-            self.cache_db = sqlite3.connect(cache_file)
-            self.cache_file = cache_file
-            self.cache_db_ps = get_paramstyle_symbol(sqlite3.paramstyle)
-            ### Ensure that the correct table/schema exists
-            self.cache_db.execute(CACHE_TABLE_SCHEMA)
-
-    def clear_memory(self):
+    def drop_cache(self):
         """
         Clear the query cache
         """
+        super(NerscJobsDb, self).drop_cache()
         self.cached_queries = {}
-
-    def close_cache(self):
-        """
-        Close a cache db and reset its intent
-        """
-        if self.cache_db is not None:
-            self.cache_db = self.cache_db.close()
-            self.cache_file = None
-
-    def save_cache(self, cache_file):
-        """
-        Commit the in-memory cache to a cache database.  Currently very
-        memory-inefficient and not good for caching giant pieces of a database
-        without something wrapping it to feed it smaller pieces.
-        """
-        ### Note the state of old cache db
-        old_cache_file = self.cache_file
-        self.close_cache()
-
-        ### Open a new cache db connection
-        self.connect_cache(cache_file)
-
-        ### Insert the contents of our query cache into the cache db
-        bulk_insert = []
-        for _, rows in self.cached_queries.iteritems():
-            bulk_insert += rows
-        # Note that we INSERT OR REPLACE here; the cache db never wins if a
-        # duplicate jobid is detected
-        self.cache_db.executemany("""
-            insert or replace into summary(stepid, hostname, start, completion, numnodes)
-            values (?,?,?,?,?)
-            """, bulk_insert)
-        self.cache_db.commit()
-
-        ### Restore old state of cache db
-        self.close_cache()
-        self.connect_cache(old_cache_file)
 
     def get_concurrent_jobs(self, start_timestamp, end_timestamp, nersc_host):
         """
@@ -194,7 +113,9 @@ class NerscJobsDb(object):
             'numjobs': 0,
         }
 
-        results = self.query(query_str, (nersc_host, start_timestamp, end_timestamp))
+        results = self.query(
+            query_str,
+            (nersc_host, start_timestamp, end_timestamp))
 
         for (_, _, this_start, this_end, nnodes) in results:
             real_start = max(this_start, start_timestamp)
@@ -220,55 +141,23 @@ class NerscJobsDb(object):
         ### FIRST: check cache for the results of this query--we can get away
         ### with this because the NERSC Jobs DB is append-only
         if cache_key in self.cached_queries and not nocache:
-            results = self.cached_queries[cache_key]
+            index_start, index_end = self.cached_queries[cache_key]
+            results = self.saved_results['summary']['rows'][index_start:index_end]
+            self.cached_queries[cache_key]
             self.last_hit = HIT_MEMORY
-        ### SECOND: check the cache database (if available)
-        elif self.cache_db is not None:
-            results = self._query_sqlite3(query_str, query_variables)
-            self.last_hit = HIT_CACHE_DB
-        ### THIRD: check the MySQL database (if available)
-        elif self.remote_db is not None:
-            results = self._query_mysql(query_str, query_variables)
-            self.last_hit = HIT_REMOTE_DB
         else:
-            raise RuntimeError('No databases available to query')
+            if 'summary' not in self.saved_results:
+                index_start = 0
+            else:
+                index_start = len(self.saved_results['summary']['rows'])
+            results = super(NerscJobsDb, self).query(
+                query_str,
+                query_variables,
+                table='summary',
+                table_schema=CACHE_TABLE_SCHEMA)
+            index_end = len(self.saved_results['summary']['rows'])
+            if not nocache:
+                self.cached_queries[cache_key] = (index_start, index_end)
 
-        if not nocache:
-            self.cached_queries[cache_key] = results
-        self.last_results = results
-
+        self.last_results = results # for debugging
         return results
-
-    def _query_sqlite3(self, query_str, query_variables):
-        """
-        Run a query against the cache database and yield the full output.  No
-        buffering, so be careful.
-        """
-        cursor = self.cache_db.cursor()
-        cursor.execute(query_str % {'ps': self.cache_db_ps}, query_variables)
-        rows = cursor.fetchall()
-        cursor.close()
-        return rows
-
-    def _query_mysql(self, query_str, query_variables):
-        """
-        Run a query against the MySQL database and yield the full output tuple.
-        No buffering, so be careful.
-
-        """
-        cursor = self.remote_db.cursor()
-        cursor.execute(query_str % {'ps': self.remote_db_ps}, query_variables)
-        rows = cursor.fetchall()
-        cursor.close()
-        return rows
-
-def get_paramstyle_symbol(paramstyle):
-    """
-    Infer the correct paramstyle for a database.paramstyle (see PEP-0249)
-    """
-    if paramstyle == 'qmark':
-        return "?"
-    elif paramstyle == 'format' or paramstyle == 'pyformat':
-        return "%s"
-    else:
-        raise Exception("Unsupported paramstyle %s" % paramstyle)
