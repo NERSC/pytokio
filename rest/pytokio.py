22833da8dd0b11ca1745c2bb7568325e310ccb93
Glenn K. Lockwood <glock@lbl.gov>
2017-09-28 16:03:58 -0700

    proof of concept for wrapping the tokio.tools.hdf5 interface

--- rest/pytokio.py
+++ rest/pytokio.py
@@ -5,23 +5,54 @@ connectors.
 """
 
 import json
+import datetime
 import bottle
+import tokio
+import tokio.connectors.hdf5
+import tokio.tools.hdf5
+import tokio.config
 
-@bottle.route('/v1/<host:re:franklin|hopper|edison|cori>/<begin:re:\d{10}>/<end:re:\d{10}>/<data:re:bulk|metadata>/')
-@bottle.route('/v1/<host:re:franklin|hopper|edison|cori>/<begin:re:\d{10}>/<end:re:\d{10}>/<data:re:bulk|metadata>')
-def base_route(host, begin, end, data):
+def tokio_tool_hdf5(method, fs, group, start, end):
     """
-    Demonstrate basic bottle integration
+    Wrap the tokio.tools.hdf5 tool
     """
-    bottle.response.content_type = 'application/json'
-    bottle.response.headers['Access-Control-Allow-Origin'] = '*'
-    response = {
-        'host': host,
-        'begin': begin,
-        'end': end,
-        'data': data,
-    }
-    return json.dumps({'status': 'success', 'response': response})
+    if method == 'group':
+        # TODO: check how much data is returned before it is returned
+        file_name = tokio.config.FSNAME_TO_H5LMT_FILE.get(fs, None)
+        if file_name is None:
+            return json.dumps({'result': 'failure', 'reason': 'unknown file name'})
+        # TODO: come up with a better way to do this (define group names in tokio.connectors.hdf5?)
+        group_name = tokio.connectors.hdf5.CONVERT_TO_V1_GROUPNAME.get(group)
+        if group_name is None:
+            return json.dumps({'result': 'failure', 'reason': 'unknown group name'})
+        datetime_start = datetime.datetime.fromtimestamp(long(start))
+        datetime_end = datetime.datetime.fromtimestamp(long(end))
+        if datetime_start >= datetime_end:
+            # error: invalid relationship between start/end time
+            return json.dumps({'result': 'failure', 'reason': 'invalid date range'})
+        result = tokio.tools.hdf5.get_dataframe_from_time_range(file_name,
+                                                                group_name,
+                                                                datetime_start,
+                                                                datetime_end)
+        return result.to_json()
+    else:
+        return json.dumps({'result': 'failure', 'reason': 'unknown method'})
+
+@bottle.get('/v1/<fs>/<tool>/<method>/<group>/<start>/<end>')
+def base_route(fs, tool, method, group, start, end):
+    """
+    Provide API into tokio.tools
+    """
+    if tool == 'hdf5':
+        result = tokio_tool_hdf5(method, fs, group, start, end)
+        if result is None:
+            bottle.response.status = 400
+            return
+        else:
+            return result
+    else:
+        bottle.response.status = 400
+        return
 
 bottle.debug(True)
 
