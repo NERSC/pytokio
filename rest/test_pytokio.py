60c188b9c3596b8357f0d448f0e2d039a130ad97
Glenn K. Lockwood <glennklockwood@gmail.com>
2017-10-13 17:32:27 -0700

    not exactly a test yet

---
+++ rest/test_pytokio.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+Test the functionality of the pytokio REST API.
+"""
+
+# Currently not implemented.  Test by hand by first launching the REST API:
+#
+#   PYTOKIO_H5LMT_BASE_DIR=$(greadlink -f ../tests/inputs)/%Y-%m-%d ./pytokio.py
+#
+# Then accessing the API using the test data:
+#
+#   http://localhost:18880/v1/hdf5/scratch2/group/osscpu/1490012544/1490016603
