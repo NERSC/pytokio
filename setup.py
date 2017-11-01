dfcaf7e8f3619995dacf6063ff94192ac1dee4ee
Glenn K. Lockwood <glock@lbl.gov>
2017-11-01 16:38:47 -0700

    make this install as a package for Spin

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ from distutils.core import setup
 setup(
     name='tokio',
     description= 'Total Knowledge of I/O',
-    version='0.0.1',
+    version='0.9',
     author='Glenn K. Lockwood',
-    author_email = 'glock@nersc.gov',
+    author_email = 'glock@lbl.gov',
     license='None',
     packages=['tokio', 'tokio.connectors', 'tokio.tools'],
     classifiers=[
         'Programming Language :: Python :: 2.7',
     ],
+    package_data={'tokio': ['site.json']},
+    include_package_data=True,
     keywords='I/O NERSC performance'
 )
