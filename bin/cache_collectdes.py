05a5e1facd4334c4eea80629c8eab7a38c4ac91d
Glenn K. Lockwood <glock@lbl.gov>
2017-11-04 00:52:00 -0700

    wip commit

--- bin/cache_collectdes.py
+++ bin/cache_collectdes.py
@@ -100,6 +100,7 @@ def commit_mem_datasets(hdf5_filename, rows, columns, datasets):
     hdf5_file = h5py.File(hdf5_filename)
 
     # Create the read rate dataset
+    dataset_name = None
     for dataset_name, dataset in datasets.iteritems():
         check_create_hdf5_dataset(hdf5_file=hdf5_file,
                                   name=dataset_name,
@@ -108,8 +109,13 @@ def commit_mem_datasets(hdf5_filename, rows, columns, datasets):
                                   compression='gzip',
                                   dtype='f8')
 
-        ### Sort the columns at this point
-        new_columns = sorted(columns)
+        ### If we're updating an existing.  Otherwise sort the columns
+        if 'columns' in hdf5_file[dataset_name].attrs['columns']:
+            new_columns = hdf5_file[dataset_name].attrs['columns']
+        else:
+            new_columns = sorted(columns)
+
+        ### Actually rearrange column data
         for new_index, column in enumerate(new_columns):
             moved_column = dataset[:, new_index]
             moved_index = columns.index(column)
@@ -120,13 +126,25 @@ def commit_mem_datasets(hdf5_filename, rows, columns, datasets):
         ###       changed; for example, maybe calculate the min/max row and
         ###       column touched, then use these as slice indices
 
+        if 'columns' not in hdf5_file[dataset_name].attrs and 'timestamps' not in hdf5_file:
+            raise Exception("Existing HDF5 has dataset %s but no timestamps" % dataset_name)
+
+        ### TODO: resume work here.  right now we are trying to implement
+        ### updating an existing HDF5 file, but the mem_dataset is not
+        ### initialized from an hdf5 file if present so the process of merging a
+        ### mem_dataset into an existing hdf5 dataset that isn't a complete
+        ### superset is complicated.  perhaps just initialize the dataset as a mem
+        ### _or_ take whatever is in the target output file and start with that?
+        raise Exception('this code path is currently incomplete')
+
         # Populate the read rate dataset and set basic metadata
         hdf5_file[dataset_name][:, :] = dataset
         hdf5_file[dataset_name].attrs['columns'] = new_columns
         print "Committed %s of shape %s" % (dataset_name, dataset.shape)
 
-        # Create the dataset that contains the timestamps which correspond to each
-        # row in the read/write datasets
+    # Create the dataset that contains the timestamps which correspond to each
+    # row in the read/write datasets
+    if dataset_name is not None:
         dataset_name = os.path.join(os.path.dirname(dataset_name), 'timestamps')
         check_create_hdf5_dataset(hdf5_file=hdf5_file,
                                   name=dataset_name,
@@ -135,6 +153,8 @@ def commit_mem_datasets(hdf5_filename, rows, columns, datasets):
 
         hdf5_file[dataset_name][:] = rows
         print "Committed %s of shape %s" % (dataset_name, dataset.shape)
+    else:
+        warnings.warn("No data to commit; HDF5 is untouched")
 
 def update_mem_datasets(pages, rows, column_map, read_dataset, write_dataset):
     """
@@ -149,39 +169,37 @@ def update_mem_datasets(pages, rows, column_map, read_dataset, write_dataset):
 
     data_volume = [0, 0]
     index_errors = 0
-    for page in pages:
-        for doc in page:
-            # basic validity checking
-            if '_source' not in doc:
-                print doc
-                warnings.warn("No _source in doc %s" % str(doc['_id']))
-                continue
-            source = doc['_source']
-            # check to see if this is from plugin:disk
-            if source['plugin'] != 'disk' or 'read' not in source:
-                continue
-            timestamp = long((dateutil.parser.parse(source['@timestamp']) - EPOCH).total_seconds())
-            t_index = (timestamp - time0) / timestep
-            s_index = column_map.get(source['hostname'])
-            # if this is a new hostname, create a new column for it
-            if s_index is None:
-                s_index = len(column_map)
-                column_map[source['hostname']] = s_index
-            # bounds checking
-            if t_index >= read_dataset.shape[0] \
-            or t_index >= write_dataset.shape[0]:
-                index_errors += 1
-                continue
-            read_dataset[t_index, s_index] = source['read']
-            write_dataset[t_index, s_index] = source['write']
+    for doc in pages:
+        # basic validity checking
+        if '_source' not in doc:
+            warnings.warn("No _source in doc")
+            print json.dumps(doc, indent=4)
+            print json.dumps(page, indent=4)
+            continue
+        source = doc['_source']
+        # check to see if this is from plugin:disk
+        if source['plugin'] != 'disk' or 'read' not in source:
+            continue
+        timestamp = long((dateutil.parser.parse(source['@timestamp']) - EPOCH).total_seconds())
+        t_index = (timestamp - time0) / timestep
+        s_index = column_map.get(source['hostname'])
+        # if this is a new hostname, create a new column for it
+        if s_index is None:
+            s_index = len(column_map)
+            column_map[source['hostname']] = s_index
+        # bounds checking
+        if t_index >= read_dataset.shape[0] \
+        or t_index >= write_dataset.shape[0]:
+            index_errors += 1
+            continue
+        read_dataset[t_index, s_index] = source['read']
+        write_dataset[t_index, s_index] = source['write']
 
-            data_volume[0] += source['read'] * timestep
-            data_volume[1] += source['write'] * timestep
-        if index_errors > 0:
-            warnings.warn("Out-of-bounds indices (%d total) were detected" % index_errors)
-        print "Added %d bytes of read, %d bytes of write" % (data_volume[0], data_volume[1])
-
-
+        data_volume[0] += source['read'] * timestep
+        data_volume[1] += source['write'] * timestep
+    if index_errors > 0:
+        warnings.warn("Out-of-bounds indices (%d total) were detected" % index_errors)
+    print "Added %d bytes of read, %d bytes of write" % (data_volume[0], data_volume[1])
 
 def init_mem_dataset(start_time, end_time, timestep=10, num_columns=10000):
     """
@@ -208,17 +226,18 @@ def check_create_hdf5_dataset(hdf5_file, name, shape, dtype, **kwargs):
     Create a dataset if it does not exist.  If it does exist and has the correct
     shape, do nothing.
     """
-    if name in hdf5_file:
-        if hdf5_file[name].shape != shape:
-            raise Exception('Dataset %s of shape %s already exists with shape %s' %
-                            name,
-                            shape,
-                            hdf5_file[name].shape)
-    else:
+    if name not in hdf5_file:
         hdf5_file.create_dataset(name=name,
                                  shape=shape,
                                  dtype=dtype,
                                  **kwargs)
+### commenting below to allow update-in-place of hdf5 files
+#   else:
+#       if hdf5_file[name].shape != shape:
+#           raise Exception('Dataset %s of shape %s already exists with shape %s' %
+#                           (name,
+#                           shape,
+#                           hdf5_file[name].shape))
 
 def run_disk_query(host, port, index, t_start, t_end):
     """
@@ -240,7 +259,9 @@ def run_disk_query(host, port, index, t_start, t_end):
     es_obj.query_and_scroll(
         query=query,
         source_filter=SOURCE_FIELDS,
-        filter_function=lambda x: x['hits']['hits']
+        filter_function=lambda x: x['hits']['hits'],
+        flush_every=50000,
+        flush_function=lambda x: x,
     )
 
     return es_obj
@@ -287,40 +308,47 @@ def cache_collectd_cli():
 
     # Parse CLI options
     parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument("start", type=str, help="start time in YYYY-MM-DDTHH:MM:SS format")
-    parser.add_argument("end", type=str, help="end time in YYYY-MM-DDTHH:MM:SS format")
+    parser.add_argument("query_start", type=str, help="start time of query in %s format" % DATE_FMT)
+    parser.add_argument("query_end", type=str, help="end time of query in %s format" % DATE_FMT)
+    parser.add_argument('--init-start', type=str, help='min timestamp if creating new output file, in %s format (default: same as start)' % DATE_FMT)
+    parser.add_argument('--init-end', type=str, help='max timestamp if creating new output file, in %s format (default: same as end)' % DATE_FMT)
     parser.add_argument('--debug', action='store_true', help="produce debug messages")
-    parser.add_argument('--num-bbnodes', type=int, default=288, help='number of expected BB nodes')
-    parser.add_argument('--timestep', type=int, default=10, help='collection frequency, in seconds')
-    parser.add_argument("-o", "--output", type=str, default='output.hdf5', help="output file")
-    parser.add_argument('-h', '--host', type=str, default="localhost", help="hostname of ElasticSearch endpoint")
-    parser.add_argument('-p', '--port', type=int, default=9200, help="port of ElasticSearch endpoint")
-    parser.add_argument('-i', '--index', type=str, default='cori-collectd-*', help='ElasticSearch index to query')
+    parser.add_argument('--num-bbnodes', type=int, default=288, help='number of expected BB nodes (default: 288)')
+    parser.add_argument('--timestep', type=int, default=10, help='collection frequency, in seconds (default: 10)')
+    parser.add_argument("-o", "--output", type=str, default='output.hdf5', help="output file (default: output.hdf5)")
+    parser.add_argument('-h', '--host', type=str, default="localhost", help="hostname of ElasticSearch endpoint (default: localhost)")
+    parser.add_argument('-p', '--port', type=int, default=9200, help="port of ElasticSearch endpoint (default: 9200)")
+    parser.add_argument('-i', '--index', type=str, default='cori-collectd-*', help='ElasticSearch index to query (default:cori-collectd-*)')
     args = parser.parse_args()
-    if not (args.start and args.end):
-        parser.print_help()
-        sys.exit(1)
 
     if args.debug:
         tokio.DEBUG = True
 
     ### Convert CLI options into datetime
     try:
-        t_start = datetime.datetime.strptime(args.start, DATE_FMT)
-        t_end = datetime.datetime.strptime(args.end, DATE_FMT)
+        query_start = datetime.datetime.strptime(args.query_start, DATE_FMT)
+        query_end = datetime.datetime.strptime(args.query_end, DATE_FMT)
+        init_start = query_start
+        init_end = query_end
+        if args.init_start:
+            init_start = datetime.datetime.strptime(args.init_start, DATE_FMT)
+        if args.init_end:
+            init_end = datetime.datetime.strptime(args.init_end, DATE_FMT)
     except ValueError:
         sys.stderr.write("Start and end times must be in format %s\n" % DATE_FMT)
         raise
 
     ### Basic input bounds checking
-    if t_start >= t_end:
-        raise Exception('t_start >= t_end')
+    if query_start >= query_end:
+        raise Exception('query_start >= query_end')
+    elif init_start >= init_end:
+        raise Exception('query_start >= query_end')
     elif args.timestep < 1:
         raise Exception('--timestep must be > 0')
 
-    es_obj = run_disk_query(args.host, args.port, args.index, t_start, t_end)
+    es_obj = run_disk_query(args.host, args.port, args.index, query_start, query_end)
 
-    pages_to_hdf5(t_start, t_end, es_obj.scroll_pages, args.timestep, args.num_bbnodes, args.output)
+    pages_to_hdf5(init_start, init_end, es_obj.scroll_pages, args.timestep, args.num_bbnodes, args.output)
 
 if __name__ == '__main__':
     cache_collectd_cli()
