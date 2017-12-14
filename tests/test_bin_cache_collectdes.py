a13a894140457a3d248e55c9e5231f99f5c3d650
Glenn K. Lockwood <glock@lbl.gov>
2017-12-13 17:10:07 -0800

    remove vestigial code

--- tests/test_bin_cache_collectdes.py
+++ tests/test_bin_cache_collectdes.py
@@ -8,19 +8,11 @@ import os
 import subprocess
 import tokiotest
 
-BINARY1 = os.path.join(tokiotest.BIN_DIR, 'cache_collectdes.py')
-BINARY2 = os.path.join(tokiotest.BIN_DIR, 'cache_collectdes_supplemental.py')
+BINARY = os.path.join(tokiotest.BIN_DIR, 'cache_collectdes.py')
 
 def test_bin_cache_collectdes():
     """
     bin/cache_collectdes.py
     """
     raise NotImplementedError
-    subprocess.check_output([BINARY1])
-
-def test_bin_cache_collectdes_supplemental():
-    """
-    bin/cache_collectdes_supplemental.py
-    """
-    raise NotImplementedError
-    subprocess.check_output([BINARY2])
+    subprocess.check_output([BINARY])
