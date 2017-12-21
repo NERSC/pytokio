c0e2d46efe1893f47da0e06a9fa58d4420fb8080
Glenn K. Lockwood <glock@lbl.gov>
2017-12-20 16:49:38 -0800

    remove permanent output files being generated

--- tests/test_bin_cache_collectdes.py
+++ tests/test_bin_cache_collectdes.py
@@ -110,15 +110,11 @@ def test_bin_cache_collectdes():
     summary0 = summarize_hdf5(h5_file)
     h5_file.close()
 
-    subprocess.call(['cp', tokiotest.TEMP_FILE.name, 'debug1.hdf5'])
-
     update_tts(tokiotest.TEMP_FILE.name)
 
     h5_file = h5py.File(tokiotest.TEMP_FILE.name, 'r')
     summary1 = summarize_hdf5(h5_file)
     h5_file.close()
-
-    subprocess.call(['cp', tokiotest.TEMP_FILE.name, 'debug2.hdf5'])
 
     num_compared = 0
     for metric in 'sums', 'shapes':
