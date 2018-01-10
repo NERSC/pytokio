9fe2b43a0f0bc8033c64a8ae83f3492791f0f5fa
Glenn K. Lockwood <glock@lbl.gov>
2018-01-10 15:57:06 -0800

    minor improvement to documentation

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
+#
+#  To install:
+#
+#    python setup.py install
+#
+#  or
+#
+#    python setup.py install --prefix=/path/to/prefix
+#
+#  To create an egg:
+#
+#    python -c "import setuptools; execfile('setup.py')" bdist_egg
+#
 from distutils.core import setup
 
 setup(
@@ -12,6 +25,5 @@ setup(
         'Programming Language :: Python :: 2.7',
     ],
     package_data={'tokio': ['site.json']},
-    include_package_data=True,
     keywords='I/O NERSC performance'
 )
