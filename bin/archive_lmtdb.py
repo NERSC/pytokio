03288995a9e8508abb462cbc4e7df4ac8d84358d
Glenn K. Lockwood <glock@lbl.gov>
2018-01-29 22:00:58 -0800

    functional archiver (but not necessarily correct!)

--- bin/archive_lmtdb.py
+++ bin/archive_lmtdb.py
@@ -6,10 +6,12 @@ Retrieve the contents of an LMT database and cache it locally.
 import os
 import datetime
 import argparse
+import h5py
 import cache_collectdes
 import tokio.connectors.lmtdb
 
 DATE_FMT = "%Y-%m-%dT%H:%M:%S"
+DATE_FMT_PRINT = "YYYY-MM-DDTHH:MM:SS"
 
 VERSION = "1"
 
@@ -64,20 +66,20 @@ def archive_ost_data(lmtdb, init_start, init_end, timestep, output_file, query_s
 
     # Initialize raw datasets - extend query by one extra timestep so we can calculate deltas
     query_end_plusplus = query_end + datetime.timedelta(seconds=timestep)
-    raw_datasets = {}
-    for group_name in group_names:
-        raw_datasets.update(cache_collectdes.init_datasets(init_start=query_start,
-                                                           init_end=query_end_plusplus,
-                                                           timestep=timestep,
-                                                           num_columns=len(columns),
-                                                           output_file=None,
-                                                           group_name=group_name,
-                                                           dataset_names=dataset_names))
+    datasets = cache_collectdes.init_datasets(init_start=query_start,
+                                              init_end=query_end_plusplus,
+                                              timestep=timestep,
+                                              num_columns=len(columns),
+                                              output_file=None,
+                                              dataset_names=dataset_names)
+
+    print datasets.keys()
 
     # Set column names
-    for dataset in raw_datasets.itervalues():
+    for dataset in datasets.itervalues():
         dataset.set_columns(columns)
 
+    # Now query the OST_DATA table to get byte counts over the query time range
     results, columns = lmtdb.get_timeseries_data('OST_DATA', query_start, query_end_plusplus)
 
     # Index the columns before building the Timeseries objects
@@ -99,17 +101,22 @@ def archive_ost_data(lmtdb, init_start, init_end, timestep, output_file, query_s
             # SQLite stores timestamps as a unicode string
             timestamp = datetime.datetime.strptime(row[idx_timestamp], "%Y-%m-%d %H:%M:%S")
         else:
-            # MySQL stores timestamps as a long (epoch)
-            timestamp = datetime.fromtimestamp(row[idx_timestamp])
+            # MySQL timestamps are automatically converted to datetime.datetime
+            timestamp = row[idx_timestamp]
         col_name = ost_id_map[row[idx_ostid]]
-        raw_datasets[schema['datatargets/readbytes']].insert_element(timestamp, col_name, row[idx_readbytes])
-        raw_datasets[schema['datatargets/writebytes']].insert_element(timestamp, col_name, row[idx_writebytes])
-        raw_datasets[schema['fullness/bytes']].insert_element(timestamp, col_name, row[idx_kbused])
-        raw_datasets[schema['fullness/inodes']].insert_element(timestamp, col_name, row[idx_inodesused])
-        raw_datasets[schema['fullness/bytestotal']].insert_element(timestamp, col_name, row[idx_kbused] + row[idx_kbfree])
-        raw_datasets[schema['fullness/inodestotal']].insert_element(timestamp, col_name, row[idx_inodesused] + row[idx_inodesfree])
+        datasets[schema['datatargets/readbytes']].insert_element(timestamp, col_name, row[idx_readbytes])
+        datasets[schema['datatargets/writebytes']].insert_element(timestamp, col_name, row[idx_writebytes])
+        datasets[schema['fullness/bytes']].insert_element(timestamp, col_name, row[idx_kbused])
+        datasets[schema['fullness/inodes']].insert_element(timestamp, col_name, row[idx_inodesused])
+        datasets[schema['fullness/bytestotal']].insert_element(timestamp, col_name, row[idx_kbused] + row[idx_kbfree])
+        datasets[schema['fullness/inodestotal']].insert_element(timestamp, col_name, row[idx_inodesused] + row[idx_inodesfree])
 
-    return raw_datasets
+    # Convert some datasets from absolute byte counts to deltas
+    for dataset_name in 'datatargets/readbytes', 'datatargets/writebytes':
+        datasets[schema[dataset_name]].dataset = matrix_to_rates(datasets[schema[dataset_name]].dataset)
+        print datasets[schema[dataset_name]].dataset_name
+
+    return datasets
 
 def matrix_to_rates(matrix):
     """
@@ -137,26 +144,34 @@ def archive_lmtdb(lmtdb, init_start, init_end, timestep, output_file, query_star
     Given a start and end time, retrieve all of the relevant contents of an LMT
     database.
     """
-    pass
+    datasets = archive_ost_data(lmtdb, init_start, init_end, timestep, output_file, query_start, query_end)
+
+    output_hdf5 = h5py.File(output_file)
+    for dataset in datasets.itervalues():
+        print "Writing out %s" % dataset.dataset_name
+        dataset.commit_dataset(output_hdf5)
+    output_hdf5.close()
+
+    if tokio.DEBUG:
+        print "Wrote output to %s" % output_file
 
 def _archive_lmtdb():
     """
     Verify functionality when connecting to a remote database
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("query_start", type=str, help="start time in %s format" % DATE_FMT)
-    parser.add_argument("query_end", type=str, help="end time in %s format" % DATE_FMT)
-    parser.add_argument('--init-start', type=str, default=None, help='min timestamp if creating new output file, in %s format (default: same as start)' % DATE_FMT)
-    parser.add_argument('--init-end', type=str, default=None, help='max timestamp if creating new output file, in %s format (default: same as end)' % DATE_FMT)
-    parser.add_argument('--debug', action='store_true', help="produce debug messages")
-    parser.add_argument('--timestep', type=int, default=10, help='collection frequency, in seconds (default: 10)')
-
     parser.add_argument("-i", "--input", type=str, default=None, help="input cache db file")
     parser.add_argument("-o", "--output", type=str, default='output.hdf5', help="output file (default: output.hdf5)")
+    parser.add_argument('--init-start', type=str, default=None, help='min timestamp if creating new output file, in %s format (default: same as start)' % DATE_FMT_PRINT)
+    parser.add_argument('--init-end', type=str, default=None, help='max timestamp if creating new output file, in %s format (default: same as end)' % DATE_FMT_PRINT)
+    parser.add_argument('--debug', action='store_true', help="produce debug messages")
+    parser.add_argument('--timestep', type=int, default=10, help='collection frequency, in seconds (default: 10)')
     parser.add_argument("--host", type=str, default=None, help="database hostname")
     parser.add_argument("--user", type=str, default=None, help="database user")
     parser.add_argument("--password", type=str, default=None, help="database password")
     parser.add_argument("--database", type=str, default=None, help="database name")
+    parser.add_argument("query_start", type=str, help="start time in %s format" % DATE_FMT_PRINT)
+    parser.add_argument("query_end", type=str, help="end time in %s format" % DATE_FMT_PRINT)
     args = parser.parse_args()
 
     if args.debug:
@@ -164,8 +179,8 @@ def _archive_lmtdb():
 
     # Convert CLI options into datetime
     try:
-        query_start = datetime.datetime.strptime(args.start, DATE_FMT)
-        query_end = datetime.datetime.strptime(args.end, DATE_FMT)
+        query_start = datetime.datetime.strptime(args.query_start, DATE_FMT)
+        query_end = datetime.datetime.strptime(args.query_end, DATE_FMT)
         init_start = query_start
         init_end = query_end
         if args.init_start:
@@ -184,7 +199,6 @@ def _archive_lmtdb():
     elif args.timestep < 1:
         raise Exception('--timestep must be > 0')
 
-
     if args.input is not None:
         lmtdb = tokio.connectors.lmtdb.LmtDb(cache_file=args.input)
     else:
@@ -194,10 +208,13 @@ def _archive_lmtdb():
             dbpassword=args.password,
             dbname=args.database)
 
-    archive_lmtdb(lmtdb, init_start, init_end, timestep, output_file, query_start, query_end)
-
-    if args.debug:
-        print "Wrote output to %s" % args.output
+    archive_lmtdb(lmtdb=lmtdb,
+                  init_start=init_start,
+                  init_end=init_end,
+                  timestep=args.timestep,
+                  output_file=args.output,
+                  query_start=query_start,
+                  query_end=query_end)
 
 if __name__ == "__main__":
     _archive_lmtdb()
