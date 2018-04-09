b68769d562af549988295c244b82e88ed9d91580
Glenn K. Lockwood <glennklockwood@gmail.com>
2018-04-08 19:39:58 -0700

    print class attributes as well

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,6 +43,20 @@ extensions = [
     'sphinx.ext.napoleon',
 ]
 
+# autoclass_content = 'both'
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = True
+napoleon_include_special_with_doc = True
+#napoleon_use_admonition_for_examples = False
+#napoleon_use_admonition_for_notes = False
+#napoleon_use_admonition_for_references = False
+napoleon_use_ivar = True
+#napoleon_use_param = True
+#napoleon_use_rtype = True
+#napoleon_use_keyword = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
