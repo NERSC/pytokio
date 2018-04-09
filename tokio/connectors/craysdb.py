a10acfa250f263fb7b0f0d44b55fa1432ed4e740
Glenn K. Lockwood <glennklockwood@gmail.com>
2018-04-08 19:54:20 -0700

    updated documentation

--- tokio/connectors/craysdb.py
+++ tokio/connectors/craysdb.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 """
-Interface with the Cray XT/XC service database to determine information about a
-node's configuration within the network fabric.
+This connection provides an interface to the Cray XT/XC service database (SDB).
+It is intended to be used to determine information about a node's configuration
+within the network fabric to provide topological information.
 """
 
 import os
@@ -13,15 +14,21 @@ import subprocess
 import mimetypes
 
 class CraySdbProc(dict):
-    """
-    Presents certain views of the Cray Service Database (SDB) as a dictionary.
-    Lazy load data to prevent unnecessary touches to the actual service
-    database.
+    """Dictionary subclass that self-populates with Cray SDB data.
 
-    This may someday become a base class for table-specific classes.
-    
+    Presents certain views of the Cray Service Database (SDB) as a
+    dictionary-like object through the Cray SDB CLI.
     """
     def __init__(self, cache_file=None):
+        """Load the processor configuration table from the SDB.
+
+        Loads the Cray processor map table using either a cached, ASCII version
+        of the table or by calling the ``xtdb2proc`` command.
+
+        Args:
+            cache_file (str, optional): Path to a cache file to load instead of
+                issuing the ``xtdb2proc`` command.
+        """
         super(CraySdbProc, self).__init__(self)
         self.cache_file = cache_file
         # Keep the order in which we parse xtdb2proc output file  
@@ -29,9 +36,11 @@ class CraySdbProc(dict):
         self.load_xtdb2proc_table()
 
     def load_xtdb2proc_table(self):
-        """
-        Load an xtdb2proc output file for a system
-        
+        """Load the xtdb2proc data for a Cray system.
+
+        Either loads a cached version of the xtdb2proc output text, or calls
+        ``xtdb2proc`` to collect that data from the Cray service database.
+        Inserts keys/values into self.
         """
         if self.cache_file is None:
             # Load directly from the Cray service database
@@ -42,7 +51,7 @@ class CraySdbProc(dict):
                     raise type(error)(error[0], "CraySDB CLI (xtdb2proc command) not found")
                 raise
             # sdb = subprocess.Popen(['xtdb2proc', '-f', '-'], stdout=subprocess.PIPE).communicate()[0]
-            self._parse_xtdb2proc_table(sdb.splitlines())
+            self.parse_xtdb2proc_table(sdb.splitlines())
         else:
             # Load a cached copy of the service database xtdb2proc table
             _, encoding = mimetypes.guess_type(self.cache_file)
@@ -50,14 +59,18 @@ class CraySdbProc(dict):
                 fp = gzip.open(self.cache_file, 'r')
             else:
                 fp = open(self.cache_file, 'r')
-            self._parse_xtdb2proc_table(fp)
+            self.parse_xtdb2proc_table(fp)
             fp.close()
 
     def __repr__(self):
-        """
+        """Serialize self in a format compatible with ``xtdb2proc``.
+
         Returns the object in the same format as the xtdb2proc output so that
-        this object can be circularly serialized and deserialized
-        
+        this object can be circularly serialized and deserialized.
+
+        Returns:
+            str: String representation of the processor mapping table in a
+            format compatible with the output of ``xtdb2proc``.
         """
         repr_result = ""
         for _, record in self.iteritems():
@@ -87,9 +100,14 @@ class CraySdbProc(dict):
     
     
     def save_cache(self, output_file=None):
-        """
-        Serialize the object in a form compatible with the output of xtdb2proc
-        
+        """Serialize self and write out to a cache file
+
+        Serializes the object into a form compatible with the output of
+        ``xtdb2proc`` and optionally stores that output to a file.
+
+        Args:
+            output_file (str, optional): Path to a file to which the serialized
+                output should be written.  If None, print to stdout.
         """
         if output_file is None:
             self._save_cache(sys.stdout)
@@ -98,14 +116,21 @@ class CraySdbProc(dict):
                 self._save_cache(fp)
 
     def _save_cache(self, output):
+        """Serialize object into a form compatible with ``xtdb2proc``.
+        """
         output.write(str(self))
 
-    #========================================================#
-        
-    def _parse_xtdb2proc_table(self, iterable):
-        """
-        Load a serialized SDB cache passed in as an iterable
+    def parse_xtdb2proc_table(self, iterable):
+        """Ingest a serialized SDB processor mapping.
 
+        Convert an iterable representation of the Cray service database
+        processor mapping into key-value pairs, then insert them into the
+        object.
+
+        Args:
+            iterable: Any iterable that contains one service database record
+                per element.  Typically a file-like object containing the output
+                of ``xtdb2proc``.
         """
         # Save the key order once
         check_keys = True
