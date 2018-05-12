f350b2ffb05317303fa190f7ded8ee24d7948f47
Glenn K. Lockwood <glock@lbl.gov>
2018-05-11 17:49:05 -0700

    feature: can filter out specific users/exes/fs; can now specify multiple limits or filters

--- bin/darshan_scoreboard.py
+++ bin/darshan_scoreboard.py
@@ -10,9 +10,12 @@ import re
 import json
 import gzip
 import mimetypes
+import collections
 import argparse
 
-def process_darshan_perfs(summary_jsons, limit_fs=None, limit_user=None, limit_exe=None):
+def process_darshan_perfs(summary_jsons,
+                          limit_fs=[], limit_user=[], limit_exe=[],
+                          exclude_fs=[], exclude_user=[], exclude_exe=[]):
     """
     Ingest the per-log file system summary contained in the summary_json file(s)
     and produce a dictionary with bytes read/written reduced on application
@@ -20,7 +23,6 @@ def process_darshan_perfs(summary_jsons, limit_fs=None, limit_user=None, limit_e
     """
 
     summary = {}
-
     for summary_json in summary_jsons:
         _, encoding = mimetypes.guess_type(summary_json)
         if encoding == 'gzip':
@@ -31,9 +33,9 @@ def process_darshan_perfs(summary_jsons, limit_fs=None, limit_user=None, limit_e
     regex_filename = re.compile(r'^([^_]+)_(.*?)_id(\d+)_.*.darshan')
 
     results = {
-        'per_user': {},
-        'per_fs': {},
-        'per_exe': {}
+        'per_user': collections.defaultdict(lambda: collections.defaultdict(int)),
+        'per_fs': collections.defaultdict(lambda: collections.defaultdict(int)),
+        'per_exe': collections.defaultdict(lambda: collections.defaultdict(int)),
     }
     for darshan_log, counters in summary.iteritems():
         darshan_log_bn = os.path.basename(darshan_log)
@@ -49,30 +51,24 @@ def process_darshan_perfs(summary_jsons, limit_fs=None, limit_user=None, limit_e
             exename = "<unknown>"
 
         # apply limits, if applicable
-
-        if limit_user and username != limit_user:
+        if (limit_user and username not in limit_user) \
+        or (limit_exe and exename not in limit_exe) \
+        or (exclude_user and username in exclude_user) \
+        or (exclude_exe and exename in exclude_exe):
             continue
-        elif limit_exe and exename != limit_exe:
-            continue
-
-        if username not in results['per_user']:
-            results['per_user'][username] = {'read_bytes': 0, 'write_bytes': 0, 'num_jobs': 0}
 
         for mount in counters.keys():
             if mount != '/':
                 # if limit_fs in play, filter at the per-record basis
-                if limit_fs and mount != limit_fs:
+                if (limit_fs and mount not in limit_fs) \
+                or (exclude_fs and mount in exclude_fs):
                     continue
                 results['per_user'][username]['read_bytes'] += counters[mount].get('read_bytes', 0)
                 results['per_user'][username]['write_bytes'] += counters[mount].get('write_bytes', 0)
                 results['per_user'][username]['num_jobs'] += 1
-                if mount not in results['per_fs']:
-                    results['per_fs'][mount] = {'read_bytes': 0, 'write_bytes': 0, 'num_jobs': 0}
                 results['per_fs'][mount]['read_bytes'] += counters[mount].get('read_bytes', 0)
                 results['per_fs'][mount]['write_bytes'] += counters[mount].get('write_bytes', 0)
                 results['per_fs'][mount]['num_jobs'] += 1
-                if exename not in results['per_exe']:
-                    results['per_exe'][exename] = {'read_bytes': 0, 'write_bytes': 0, 'num_jobs': 0}
                 results['per_exe'][exename]['read_bytes'] += counters[mount].get('read_bytes', 0)
                 results['per_exe'][exename]['write_bytes'] += counters[mount].get('write_bytes', 0)
                 results['per_exe'][exename]['num_jobs'] += 1
@@ -107,10 +103,10 @@ def print_top(categorized_data, max_show=10):
             if displayed > max_show:
                 break
             print "%2d. %40.40s %10.1f %10.1f %8d" % (displayed,
-                                                  winner_str,
-                                                  rankings[winner]['read_bytes'] / 2.0**30,
-                                                  rankings[winner]['write_bytes'] / 2.0**30,
-                                                  rankings[winner]['num_jobs'])
+                                                      winner_str,
+                                                      rankings[winner]['read_bytes'] / 2.0**30,
+                                                      rankings[winner]['write_bytes'] / 2.0**30,
+                                                      rankings[winner]['num_jobs'])
         categories += 1
 
 def main(argv=None):
@@ -118,18 +114,40 @@ def main(argv=None):
     CLI wrapper around process_darshan_perfs()
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("summaryjson", type=str, nargs='+', help="json output of darshan_per_fs_bytes.py")
-    parser.add_argument("--json", action='store_true', help="output in json format")
-    parser.add_argument("--max-show", type=int, default=10, help="show top N users, apps, file systems")
-    parser.add_argument("--limit-fs", type=str, default=None, help="only process data targeting this file system")
-    parser.add_argument("--limit-user", type=str, default=None, help="only process logs generated by this user")
-    parser.add_argument("--limit-exe", type=str, default=None, help="only process logs generated by this binary")
+    parser.add_argument("summaryjson", type=str, nargs='+',
+                        help="json output of darshan_per_fs_bytes.py")
+    parser.add_argument("--json", action='store_true',
+                        help="output in json format")
+    parser.add_argument("--max-show", type=int, default=10,
+                        help="show top N users, apps, file systems")
+    group_fs = parser.add_mutually_exclusive_group()
+    group_fs.add_argument("--limit-fs", type=str, default=None,
+                          help="only process data targeting this file system")
+    group_fs.add_argument("--exclude-fs", type=str, default=None,
+                          help="exclude data targeting this file system")
+    group_user = parser.add_mutually_exclusive_group()
+    group_user.add_argument("--limit-user", type=str, default=None,
+                            help="only process logs generated by this user")
+    group_user.add_argument("--exclude-user", type=str, default=None,
+                            help="exclude logs generated by this user")
+    group_exe = parser.add_mutually_exclusive_group()
+    group_exe.add_argument("--limit-exe", type=str, default=None,
+                           help="only process logs generated by this binary")
+    group_exe.add_argument("--exclude-exe", type=str, default=None,
+                           help="exclude logs generated by this binary")
+
     args = parser.parse_args(argv)
 
-    results = process_darshan_perfs(args.summaryjson,
-                                    limit_fs=args.limit_fs,
-                                    limit_user=args.limit_user,
-                                    limit_exe=args.limit_exe)
+    kwargs = {
+        'limit_user': args.limit_user.split(',') if args.limit_user else [],
+        'limit_fs': args.limit_fs.split(',') if args.limit_fs else [],
+        'limit_exe': args.limit_exe.split(',') if args.limit_exe else [],
+        'exclude_user': args.exclude_user.split(',') if args.exclude_user else [],
+        'exclude_fs': args.exclude_fs.split(',') if args.exclude_fs else [],
+        'exclude_exe': args.exclude_exe.split(',') if args.exclude_exe else [],
+    }
+
+    results = process_darshan_perfs(args.summaryjson, **kwargs)
     if args.json:
         print json.dumps(results, indent=4, sort_keys=True)
     else:
--- tests/test_bin_darshan_scoreboard.py
+++ tests/test_bin_darshan_scoreboard.py
@@ -14,11 +14,12 @@ import tokiobin.darshan_scoreboard
 
 ### For tests that base all tests off of the sample Darshan log
 SAMPLE_DARSHAN_LOGS = glob.glob(os.path.join(os.getcwd(), 'inputs', '*.darshan'))
+LOGS_FROM_DIR = glob.glob(os.path.join(tokiotest.SAMPLE_DARSHAN_LOG_DIR, '*', '*', '*', '*.darshan'))
+FILTER_FOR_EXE = ['vpicio_uni', 'dbscan_read']
 
 @tokiotest.needs_darshan
 def test_input_dir():
-    """
-    bin/summarize_darshanlogs.py with input dir
+    """bin/summarize_darshanlogs.py with input dir
     """
     # Need lots of error/warning suppression since our input dir contains a ton of non-Darshan logs
     warnings.filterwarnings('ignore')
@@ -31,8 +32,7 @@ def test_input_dir():
 
 @tokiotest.needs_darshan
 def test_input_file():
-    """
-    bin/summarize_darshanlogs.py with one input log
+    """bin/summarize_darshanlogs.py with one input log
     """
     tokiotest.check_darshan()
     argv = [SAMPLE_DARSHAN_LOGS[0]]
@@ -43,8 +43,7 @@ def test_input_file():
 
 @tokiotest.needs_darshan
 def test_input_files():
-    """
-    bin/summarize_darshanlogs.py with multiple input logs
+    """bin/summarize_darshanlogs.py with multiple input logs
     """
     tokiotest.check_darshan()
     argv = SAMPLE_DARSHAN_LOGS
@@ -55,8 +54,7 @@ def test_input_files():
 
 @tokiotest.needs_darshan
 def test_multithreaded():
-    """
-    bin/summarize_darshanlogs.py --threads
+    """bin/summarize_darshanlogs.py --threads
     """
     tokiotest.check_darshan()
     argv = ['--threads', '4'] + SAMPLE_DARSHAN_LOGS
@@ -67,10 +65,9 @@ def test_multithreaded():
 
 @nose.tools.with_setup(tokiotest.create_tempfile, tokiotest.delete_tempfile)
 def test_scoreboard():
+    """bin/darshan_scoreboard.py ascii output
     """
-    bin/darshan_scoreboard.py ascii output
-    """
-    argv = ['--output', tokiotest.TEMP_FILE.name] + SAMPLE_DARSHAN_LOGS
+    argv = ['--output', tokiotest.TEMP_FILE.name] + LOGS_FROM_DIR
     print "Executing:", " ".join(argv)
     tokiotest.run_bin(tokiobin.summarize_darshanlogs, argv)
 
@@ -81,10 +78,9 @@ def test_scoreboard():
 
 @nose.tools.with_setup(tokiotest.create_tempfile, tokiotest.delete_tempfile)
 def test_scoreboard_json():
+    """bin/darshan_scoreboard.py --json
     """
-    bin/darshan_scoreboard.py --json
-    """
-    argv = ['--output', tokiotest.TEMP_FILE.name] + SAMPLE_DARSHAN_LOGS
+    argv = ['--output', tokiotest.TEMP_FILE.name] + LOGS_FROM_DIR
     print "Executing:", " ".join(argv)
     tokiotest.run_bin(tokiobin.summarize_darshanlogs, argv)
 
@@ -92,4 +88,124 @@ def test_scoreboard_json():
     print "Executing:", " ".join(argv)
     output_str = tokiotest.run_bin(tokiobin.darshan_scoreboard, argv)
     decoded_result = json.loads(output_str)
+    print "Result:", decoded_result
     assert len(decoded_result) > 0
+
+@nose.tools.with_setup(tokiotest.create_tempfile, tokiotest.delete_tempfile)
+def test_scoreboard_limit_user():
+    """bin/darshan_scoreboard.py --limit-user
+    """
+    argv = ['--output', tokiotest.TEMP_FILE.name] + LOGS_FROM_DIR
+    print "Executing:", " ".join(argv)
+    tokiotest.run_bin(tokiobin.summarize_darshanlogs, argv)
+
+    argv = ['--json', '--limit-user', tokiotest.SAMPLE_DARSHAN_LOG_USER, tokiotest.TEMP_FILE.name]
+    print "Executing:", " ".join(argv)
+    output_str = tokiotest.run_bin(tokiobin.darshan_scoreboard, argv)
+    decoded_result = json.loads(output_str)
+    print "Result:", decoded_result
+    assert decoded_result['per_user']
+
+@nose.tools.with_setup(tokiotest.create_tempfile, tokiotest.delete_tempfile)
+def test_scoreboard_exclude_user():
+    """bin/darshan_scoreboard.py --exclude-user
+    """
+    argv = ['--output', tokiotest.TEMP_FILE.name] + LOGS_FROM_DIR
+    print "Executing:", " ".join(argv)
+    tokiotest.run_bin(tokiobin.summarize_darshanlogs, argv)
+
+    argv = ['--json', '--exclude-user', "%s" % tokiotest.SAMPLE_DARSHAN_LOG_USER, tokiotest.TEMP_FILE.name]
+    print "Executing:", " ".join(argv)
+    output_str = tokiotest.run_bin(tokiobin.darshan_scoreboard, argv)
+    decoded_result = json.loads(output_str)
+    print "Result:", decoded_result
+    assert not decoded_result['per_user']
+    assert not decoded_result['per_exe']
+    assert not decoded_result['per_fs']
+
+@nose.tools.with_setup(tokiotest.create_tempfile, tokiotest.delete_tempfile)
+def test_scoreboard_limit_fs():
+    """bin/darshan_scoreboard.py --limit-fs
+    """
+    argv = ['--output', tokiotest.TEMP_FILE.name] + LOGS_FROM_DIR
+    print "Executing:", " ".join(argv)
+    tokiotest.run_bin(tokiobin.summarize_darshanlogs, argv)
+
+    argv = ['--json', '--limit-fs', tokiotest.SAMPLE_DARSHAN_ALL_MOUNTS, tokiotest.TEMP_FILE.name]
+    print "Executing:", " ".join(argv)
+    output_str = tokiotest.run_bin(tokiobin.darshan_scoreboard, argv)
+    decoded_result = json.loads(output_str)
+    print "Result:", decoded_result
+    assert decoded_result['per_user']
+    assert decoded_result['per_fs']
+    assert decoded_result['per_exe']
+
+@nose.tools.with_setup(tokiotest.create_tempfile, tokiotest.delete_tempfile)
+def test_scoreboard_exclude_fs():
+    """bin/darshan_scoreboard.py --exclude-fs
+    """
+    argv = ['--output', tokiotest.TEMP_FILE.name] + LOGS_FROM_DIR
+    print "Executing:", " ".join(argv)
+    tokiotest.run_bin(tokiobin.summarize_darshanlogs, argv)
+
+    argv = ['--json', '--exclude-fs', "%s" % tokiotest.SAMPLE_DARSHAN_ALL_MOUNTS, tokiotest.TEMP_FILE.name]
+    print "Executing:", " ".join(argv)
+    output_str = tokiotest.run_bin(tokiobin.darshan_scoreboard, argv)
+    decoded_result = json.loads(output_str)
+    print decoded_result
+    assert not decoded_result['per_user']
+    assert not decoded_result['per_exe']
+    assert not decoded_result['per_fs']
+
+@nose.tools.with_setup(tokiotest.create_tempfile, tokiotest.delete_tempfile)
+def test_scoreboard_limit_exe():
+    """bin/darshan_scoreboard.py --limit-exe
+    """
+    argv = ['--output', tokiotest.TEMP_FILE.name] + LOGS_FROM_DIR
+    print "Executing:", " ".join(argv)
+    tokiotest.run_bin(tokiobin.summarize_darshanlogs, argv)
+
+    # try to get back a few specific exes that we know exist (FILTER_FOR_EXE)
+    for appname in FILTER_FOR_EXE + [','.join(FILTER_FOR_EXE)]:
+        argv = ['--json', '--limit-exe', appname, tokiotest.TEMP_FILE.name]
+        print "Executing:", " ".join(argv)
+        output_str = tokiotest.run_bin(tokiobin.darshan_scoreboard, argv)
+        decoded_result = json.loads(output_str)
+        print "Result:", decoded_result
+
+        wanted_apps = appname.split(',')
+        # make sure that we got back the same number of apps as we queried for
+        assert len(decoded_result['per_exe']) == len(wanted_apps)
+
+        # make sure that each app we wanted is defined in the results
+        for wanted_app in wanted_apps:
+            assert decoded_result['per_exe'][wanted_app]
+
+@nose.tools.with_setup(tokiotest.create_tempfile, tokiotest.delete_tempfile)
+def test_scoreboard_exclude_exe():
+    """bin/darshan_scoreboard.py --exclude-exe
+    """
+    argv = ['--output', tokiotest.TEMP_FILE.name] + LOGS_FROM_DIR
+    print "Executing:", " ".join(argv)
+    tokiotest.run_bin(tokiobin.summarize_darshanlogs, argv)
+
+    # get a reference dataset without anything removed
+    argv = ['--json', tokiotest.TEMP_FILE.name]
+    print "Executing:", " ".join(argv)
+    output_str = tokiotest.run_bin(tokiobin.darshan_scoreboard, argv)
+    reference_result = json.loads(output_str)
+    print "Result:", reference_result 
+
+    for appname in FILTER_FOR_EXE + [','.join(FILTER_FOR_EXE)]:
+        argv = ['--json', '--exclude-exe', appname, tokiotest.TEMP_FILE.name]
+        print "Executing:", " ".join(argv)
+        output_str = tokiotest.run_bin(tokiobin.darshan_scoreboard, argv)
+        decoded_result = json.loads(output_str)
+        print "Result:", decoded_result
+
+        # make sure the app is not included in the results
+        assert appname not in decoded_result['per_exe']
+
+        # make sure that we successfully removed something that was present in
+        # the unfiltered reference
+        assert len(decoded_result['per_exe']) < len(reference_result['per_exe'])
