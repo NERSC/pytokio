59c40f112c49a51ca019d6113226535d102d881c
Glenn K. Lockwood <glock@lbl.gov>
2018-08-02 15:58:28 -0700

    fixed residual issues with run environment on macos

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
 # For non-GNU systems
-if which greadlink > /dev/null
-then
-    READLINK=greadlink
-else
-    READLINK=readlink
-fi
+READLINK=${READLINK-readlink}
 
 # Always run tests in the test directory since site.json contains relative paths
-TEST_DIR="$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
+TEST_DIR="$(dirname $($READLINK -f ${BASH_SOURCE[0]}))"
 cd "$TEST_DIR"
 
 # This is required to work around a known bug in pytokio
@@ -17,5 +12,6 @@ export TZ=America/Los_Angeles
 
 # nosetests cannot reliably pass environment variables between tests
 export PYTOKIO_CONFIG="${TEST_DIR}/site.json"
+echo "Test environment will load configuration from $PYTOKIO_CONFIG"
 
 nosetests --cover-package=tokio,tokiobin $@
