2ea93915796b212f07f0e0e5556b2ccfdeceb227
Glenn K. Lockwood <glock@lbl.gov>
2018-08-27 17:23:31 -0700

    WIP: both backends work

--- tests/test_tools_jobinfo.py
+++ tests/test_tools_jobinfo.py
@@ -1,3 +1,5 @@
+"""Test jobinfo and all supported backends
+"""
 import tokio.tools.jobinfo
 import tokiotest
 
@@ -8,24 +10,23 @@ def test_get_job_startend_slurm():
     start, end = tokio.tools.jobinfo.get_job_startend(
         jobid=tokiotest.SAMPLE_DARSHAN_JOBID,
         cache_file=tokiotest.SAMPLE_SLURM_CACHE_FILE)
-
     print start, end
     assert start
     assert end
     assert start <= end
 
-def test_get_job_startend_slurm():
+def test_get_job_startend_nerscjobsdb():
     """tools.jobinfo.get_job_startend, NerscJobsDb
     """
     tokio.config.CONFIG["jobinfo_jobid_providers"] = ["nersc_jobsdb"]
     start, end = tokio.tools.jobinfo.get_job_startend(
-        jobid=tokiotest.SAMPLE_DARSHAN_JOBID)
-
+        jobid=tokiotest.SAMPLE_DARSHAN_JOBID,
+        cache_file=tokiotest.SAMPLE_NERSCJOBSDB_FILE)
     print start, end
     assert start
     assert end
     assert start <= end
 
-
 if __name__ == "__main__":
-    test_get_job_startend()
+    test_get_job_startend_slurm()
+    test_get_job_startend_nerscjobsdb()
