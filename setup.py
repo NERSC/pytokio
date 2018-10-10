25022533dbb67659e5946da6c9a6375ae8fddfb2
Glenn K. Lockwood <glock@lbl.gov>
2018-10-09 17:29:32 -0700

    embed requirements directly in setup.py

--- setup.py
+++ setup.py
@@ -22,8 +22,15 @@ import os
 import re
 import glob
 
-DOCLINES = (__doc__ or '').split("\n")
+REQUIREMENTS = [
+    "h5py>=2.7",
+    "matplotlib>=2.0.0",
+    "numpy>=1.13",
+    "pandas>=0.20",
+    "scipy>=0.19",
+]
 
+DOCLINES = (__doc__ or '').split("\n")
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 if os.path.exists('MANIFEST'):
@@ -66,7 +73,7 @@ def setup_package():
         packages=['tokio', 'tokio.connectors', 'tokio.tools', 'tokio.analysis'],
         scripts=include_scripts, # TODO: convert to console_scripts
         platforms=["Linux", "MacOS-X"],
-        install_requires=open(os.path.join(BASE_DIR, 'requirements.txt'), 'r').readlines(),
+        install_requires=REQUIREMENTS,
         python_requires=">=2.7",
         classifiers=[
             'Intended Audience :: Science/Research',
