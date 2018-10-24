2eccad6c032dc918f29d6b3ce2200efee82ce580
Glenn K. Lockwood <glock@lbl.gov>
2018-10-24 16:59:16 -0700

    slightly better documentation for users

--- tokio/cli/__init__.py
+++ tokio/cli/__init__.py
@@ -3,4 +3,7 @@ pytokio implements its command-line tools within this package.  Each such CLI
 tool either implements some useful analysis on top of pytokio connectors,
 tools, or analysis or maps some of the internal python APIs to command-line
 arguments.
+
+Most of these tools implement a ``--help`` option to explain the command-line
+options.
 """
