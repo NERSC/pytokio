fc0aa00330a8d0ef75667617cd129d87167d37de
Glenn K. Lockwood <glock@lbl.gov>
2018-12-10 17:04:20 -0800

    add helper function to convert bytes to human-readable units

--- tokio/common.py
+++ tokio/common.py
@@ -6,6 +6,27 @@ import json
 import datetime
 import numpy
 
+UNITS = [
+    (2**80, "YiB"),
+    (2**70, "ZiB"),
+    (2**60, "EiB"),
+    (2**50, "PiB"),
+    (2**40, "TiB"),
+    (2**30, "GiB"),
+    (2**20, "MiB"),
+    (2**10, "KiB")
+]
+UNITS_10 = [
+    (10**24, "YiB"),
+    (10**21, "ZiB"),
+    (10**18, "EiB"),
+    (10**15, "PiB"),
+    (10**12, "TiB"),
+    (10**9, "GiB"),
+    (10**6, "MiB"),
+    (10**3, "KiB")
+]
+
 class ConfigError(RuntimeError):
     pass
 
@@ -36,3 +57,27 @@ class JSONEncoder(json.JSONEncoder):
         elif isinstance(obj, numpy.int64):
             return int(obj)
         return json.JSONEncoder.default(self, obj)
+
+def humanize_bytes(bytect, base10=False, fmt="%.1f %s"):
+    """Converts bytes into human-readable units
+
+    Args:
+        bytect (int): Number of bytes
+        base10 (bool): Convert to base-10 units (MB, GB, etc) if True
+        fmt (str or None): Format of string to return; must contain %f/%d and %s
+            for the quantity and units, respectively.
+
+    Returns:
+        str: Quantity and units expressed in a human-readable quantity
+    """
+
+    if base10:
+        units = UNITS_10
+    else:
+        units = UNITS
+
+    for unit in units:
+        if bytect >= unit[0]:
+            return fmt % (bytect / unit[0], unit[1])
+
+    return fmt % (bytect, "bytes" if bytect != 1 else "byte")
